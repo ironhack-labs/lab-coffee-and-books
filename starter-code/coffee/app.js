5568cec82ea453d6b26f2555656b2f839da4c1c3
Daniele <daniele.germani.85@gmail.com>
2017-07-12 15:42:57 +0200

    create main-layout

--- starter-code/coffee/app.js
+++ starter-code/coffee/app.js
@@ -19,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(expressLayouts);
-app.set("layout", "layouts-main-layout");
+app.set("layout", "layouts/main-layout");
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -28,8 +28,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
 app.use('/api', api);
+app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
