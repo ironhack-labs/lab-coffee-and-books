2cb6613ed8a7950242a78e00d99f330b7ae17c1e
Frances Morales Velilla <frances0688@gmail.com>
2017-09-13 12:30:36 +0200

    Set current location

--- starter-code/public/javascripts/index.js
+++ starter-code/public/javascripts/index.js
@@ -9,33 +9,39 @@ function startMap() {
         center: ironhackBCN
       }
     );
-  }
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+          const user_location = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          
+          // Center map with user location
+          map.setCenter(user_location);
+          
+          // Add a marker for your user location
+          var ironHackBCNMarker = new google.maps.Marker({
+            position: {
+              lat: user_location.lat, 
+              lng: user_location.lng
+            },
+            map: map,
+            title: "You are here"
+          });
+          
+        }, function () {
+          console.log('Error in the geolocation service.');
+        });
+    } else {
+        console.log('Browser does not support geolocation.');
+    } 
+}   
+
   
 startMap();
 
 
-if (navigator.geolocation) {
-    
-      // Get current position
-      // The permissions dialog will popup
-      navigator.geolocation.getCurrentPosition(function (position) {
-        // Create an object to match 
-        // google's Lat-Lng object format
-        const center = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-        console.log('center: ', center)
-        // User granted permission
-        // Center the map in the position we got
-      }, function () {
-        // If something else goes wrong
-        console.log('Error in the geolocation service.');
-      });
-    } else {
-      // Browser says: Nah! I do not support this.
-      console.log('Browser does not support geolocation.');
-    }
+
     
 
 
