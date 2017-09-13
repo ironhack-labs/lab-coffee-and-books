8adbb8526443d3e8e04ecfdd77df527cb9f0d0a1
Jennie Dalgren <jennie.dalgren@gmail.com>
2017-09-13 12:44:20 +0200

    changed to only one model

--- starter-code/models/shop.js
+++ starter-code/models/shop.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 
-const coffeeshopSchema = new Schema({
+const shopSchema = new Schema({
   name: String,
   description: String,
   location: { type: { type: String },
   coordinates: [Number] }
 });
 
-coffeeshopSchema.index({ location: '2dsphere' });
-const Coffeeshop = mongoose.model('User', coffeeshopSchema);
+shopSchema.index({ location: '2dsphere' });
+const Shop = mongoose.model('User', shopSchema);
 
-module.exports = Coffeeshop;
+module.exports = Shop;
