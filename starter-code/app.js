a6e080ad5c0e7ec0a3987df8327b7069d6f9d951
Frances Morales Velilla <frances0688@gmail.com>
2017-09-13 12:26:12 +0200

    geolocation

--- starter-code/app.js
+++ starter-code/app.js
@@ -41,5 +41,8 @@ app.use(function(err, req, res, next) {
     res.render('error');
   });
   
+
+
+  
   module.exports = app;
   
--- starter-code/public/javascripts/index.js
+++ starter-code/public/javascripts/index.js
@@ -12,3 +12,38 @@ function startMap() {
   }
   
 startMap();
+
+
+if (navigator.geolocation) {
+    
+      // Get current position
+      // The permissions dialog will popup
+      navigator.geolocation.getCurrentPosition(function (position) {
+        // Create an object to match 
+        // google's Lat-Lng object format
+        const center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        console.log('center: ', center)
+        // User granted permission
+        // Center the map in the position we got
+      }, function () {
+        // If something else goes wrong
+        console.log('Error in the geolocation service.');
+      });
+    } else {
+      // Browser says: Nah! I do not support this.
+      console.log('Browser does not support geolocation.');
+    }
+    
+
+
+var myMarker = new google.maps.Marker({
+    position: {
+        lat: 41.3977381, 
+        lng: 2.190471916
+    },
+    map: map,
+    title: "I'm here"
+  });
