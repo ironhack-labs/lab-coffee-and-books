92958913e8813ac59d3bcc2f024c94963fe6a96f
gemaderus <hi@gemaderus.com>
2017-11-16 00:44:47 +0100

    Clean code and finished.

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,67 +1,17 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const index = require('./routes/index');
+const places = require('./routes/places');
+const mongoose = require('mongoose');
 
-var index = require('./routes/index');
-// var users = require('./routes/users');
+const app = express();
 
-var app = express();
-
-//mongoose configuration
-const mongoose = require("mongoose");
-
-const dbName = "mongodb://localhost/coffe-store";
-mongoose.connect(dbName, {useMongoClient:true})
-        .then(() => debug(`Connected to db: ${dbName}`));
-
-//require the user model
-const Store = require("./models/Store");
-const session       = require("express-session");
-const MongoStore = require("connect-mongo")(session);
-const bcrypt        = require("bcrypt");
-const passport      = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const flash = require("connect-flash");
-
-
-//initialize passport and session here
-passport.serializeUser((user, cb) => {
-  cb(null, user._id);
-});
-
-passport.deserializeUser((id, cb) => {
-  User.findOne({ "_id": id }, (err, user) => {
-    if (err) { return cb(err); }
-    cb(null, user);
-  });
-});
-
-passport.use(new LocalStrategy((username, password, next) => {
-  User.findOne({ username }, (err, user) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return next(null, false, { message: "Incorrect username" });
-    }
-    if (!bcrypt.compareSync(password, user.password)) {
-      return next(null, false, { message: "Incorrect password" });
-    }
-
-    return next(null, user);
-  });
-}));
-app.use(session({
-  secret: "our-passport-local-strategy-app",
-  resave: true,
-  saveUninitialized: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
+//connect to db
+mongoose.connect('mongodb://localhost/coffee');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -76,24 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-// app.use('/users', users);
-
-app.use(session({
-  secret: "basic-auth-secret",
-  cookie: { maxAge: 60*60*24*2 }, // 2 days
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    ttl: 24 * 60 * 60 // 1 day
-  })
-}));
-app.use((req,res,next) =>{
-  res.locals.user = req.session.currentUser;
-  next();
-});
-
-// require in the routers
-// app.use('/', siteController);
-
+app.use('/places', places);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,94 +1,9 @@
-const express = require('express');
-const router = express.Router();
-const Store = require('../models/Store');
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
-router.route('/')
-	.get((req, res, next) => {
-	  Store.find((error, stores) => {
-	  	if (error) {
-	  		next(error);
-	  	} else {
-	  		res.render('index', { stores });
-	  	}
-	  })
-	})
-
-  .post((req, res, next) => {
-    const newStore = {
-      name: req.body.name,
-      type: req.body.type,
-      location: req.body.location,
-      city: req.body.city,
-      latitude: req.body.latitude,
-      longitude: req.body.longitude,
-    };
-    console.log(newStore);
-  	const store = new Store(newStore);
-
-  	store.save((error) => {
-  		if (error) {
-  			next(error);
-  		} else {
-  			res.redirect('/');
-  		}
-  	})
-
-  });
-
-router.route('/new')
-	.get((req, res, next) => {
-		res.render('new');
-	});
-
-router.route('/:_id')
-	.get((req, res, next) => {
-		Store.findById(req.params.store_id, (error, store) => {
-			if (error) {
-				next(error);
-			} else {
-				res.render('show', {store});
-			}
-		})
-	})
-	.post((req, res, next) => {
-		Store.findById(req.params.store_id, (error, store) => {
-			if (error) {
-				next(error);
-			} else {
-				store.name = req.body.name;
-				store.type = req.body.type;
-				store.save((error) => {
-		  		if (error) {
-		  			next(error);
-		  		} else {
-		  			res.redirect('/');
-		  		}
-		  	})
-			}
-		})
-	});
-
-router.route('/_id/edit')
-	.get((req, res, next) => {
-		Store.findById(req.params.store_id, (error, store) => {
-			if (error) {
-				next(error);
-			} else {
-				res.render('update', { store });
-			}
-		})
-	});
-
-router.route('/:id/delete')
-	.get((req, res, next) => {
-		Store.remove({ _id: req.params.store_id }, function(error, store) {
-	    if (error) {
-	    	next(error)
-	    } else {
-	    	res.redirect('/')
-	    }
-    });
-	});
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Coffee and books' });
+});
 
 module.exports = router;
