03154950101417fc679f70911406821ebd938dc0
pablohk <pablusmmid@gmail.com>
2017-11-15 21:25:05 +0100

    pain map

---
+++ starter-code/coffie-books/public/javascripts/main.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+  const sol = {
+    lat: 40.417080,
+    lng: -3.703612
+  };
+const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: sol
+  });
+
+};
