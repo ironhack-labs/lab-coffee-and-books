d6e0b351de795378118732f8c28e9f0459ec153f
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-02-07 16:33:24 +0100

    map is working, can't figure out how to access database to make the markers

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,7 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const MongoDB = require('mongodb');
+
+const mongoClient = MongoDB.MongoClient;
 
 const Place = require('../models/place.js');
+
+const url = `mongodb://localhost/coffee-and-books`;
+
+mongoClient.connect(url, (error, db) => {
+  if (error) {
+    console.log('Error trying to connect to the Database');
+    console.log(error);
+  } else {
+    console.log('Connection established correctly!! 😬');
+  }
+});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -31,6 +45,13 @@ router.post('/add-place', (req, res, next) => {
 });
 
 router.get('/map', (req, res, next) => {
-  res.render('map');
+  let placeInfo = Place.find({}, (err, places) => {
+    if (err) {
+      return next(err);
+    }
+  }).toArray();
+  console.log(placeInfo);
+
+  res.render('map', placeInfo);
 });
 module.exports = router;
