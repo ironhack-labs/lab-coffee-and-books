2fa14550b1e251769ab0572cb883d7e47cd57d99
davidalexteixeira <david5teixeira@gmail.com>
2018-02-07 14:44:25 +0100

    server up and running

--- starter-code/book-coffee/app.js
+++ starter-code/book-coffee/app.js
@@ -1,19 +1,19 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+'use strict';
+
+// -- require npm packages
+
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+// -- require your own modules (router, models)
+const index = require('./routes/index');
+const users = require('./routes/users');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// -- create app connect to db
 
 mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost/database-name', {
@@ -21,31 +21,35 @@ mongoose.connect('mongodb://localhost/database-name', {
   reconnectTries: Number.MAX_VALUE
 });
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// -- setup the app
+const app = express();
+
+// -- view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// -- configure middlewares (static, session, cookies, body, ...)
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+// -- routes
 app.use('/', index);
 app.use('/users', users);
 
 // -- 404 and error handler
 
-// NOTE: requires a views/not-found.ejs template
 app.use((req, res, next) => {
   res.status(404);
   res.render('not-found');
 });
 
-// NOTE: requires a views/error.ejs template
 app.use((err, req, res, next) => {
-  // always log the error
   console.error('ERROR', req.method, req.path, err);
 
-  // only render if the error ocurred before sending the response
   if (!res.headersSent) {
     res.status(500);
     res.render('error');
