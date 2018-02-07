ae2a92086cd5af6322a47bcc1482979d488c2ed5
Ana Recuero <anarecsa@gmail.com>
2018-02-07 16:05:09 +0100

    view list and edit places

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -29,4 +29,40 @@ router.post("/create", (req,res,next) => {
   });
 });
 
+router.get("/show", (req,res) => {
+  Place.find({}).exec((err, list) => {
+    res.render("show", {list});
+  });
+});
+
+router.get("/edit/:id", (req, res) =>{
+  const id = req.params.id;
+  Place.findById(id).exec((err, place) => {
+    res.render("edit", {place});
+  });
+})
+
+router.post("/edit/:id", (req,res, next) =>{
+  const id = req.params.id;
+  Place.findById(id).exec((err,place) => {
+    if (err){
+      res.redirect("/show");
+      return
+    };
+    const updatePlace = {
+      type: req.body.type,
+      name: req.body.name,
+      location : {
+        lat: req.body.lat,
+        lng: req.body.lng
+      }
+    };
+
+    Place.findByIdAndUpdate(id, updatePlace, (err, place) => {
+      if (err){ return next(err); }
+      res.redirect("/show");
+    });
+  })
+})
+
 module.exports = router;
