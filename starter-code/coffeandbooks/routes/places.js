59a98cf00649b478104babaccd78e72863c63462
Luis Herrera <la.herrera457@gmail.com>
2018-02-07 15:06:36 +0100

    places being saved in the db

---
+++ starter-code/coffeandbooks/routes/places.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+
+const Place = require('../models/places');
+
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+router.get('/create', (req, res, next) => {
+  const data = {
+    name: '',
+    description: '',
+    latitude: '',
+    longitude: ''
+  };
+  res.render('form', data);
+});
+
+router.post('/create', (req, res, next) => {
+  const placeInfo = {
+    name: req.body.name,
+    description: req.body.description,
+    location: {
+      type: 'Point',
+      coordinates: [Number(req.body.latitude), Number(req.body.longitude)]
+    }
+  };
+  const place = new Place(placeInfo);
+  place.save((error) => {
+    if (error) {
+      return next(error);
+    }
+    res.redirect('/');
+    // todo data validation
+  });
+});
+
+module.exports = router;
