bdfec7eb35160a72ab96c78114d68d1741f40a8b
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-04-17 21:11:31 +0200

    Added fixed files

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,4 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
   console.log('IronGenerator JS imported successfully!');
 
+    const ironhack = {
+      lat: 40.392088, 
+      lng: -3.698473
+    };
+    
+    const markers = []
+    
+    const map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 13,
+      center: ironhack
+    });
+  
+    console.log(window.places);
 }, false);
