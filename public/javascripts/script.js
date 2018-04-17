aedf84efb3da15f280fd099d3f810975781173aa
luisnueda <nueda.luis@gmail.com>
2018-04-17 21:33:55 +0200

    model

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  console.log('IronGenerator JS imported successfully!');
+  // main.js
+function startMap() {
+  const ironhackBCN = {
+    lat: 41.3977381,
+    lng: 2.190471916
+  };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: ironhackBCN
+  });
+  const myMarker = new google.maps.Marker({
+    position: {
+      lat: 41.3977381,
+      lng: 2.190471916
+    },
+    map: map,
+    title: "I'm here"
+  });
+}
+
+
+startMap();
+
 
 }, false);
