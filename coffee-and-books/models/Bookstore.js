1dfe97b606e73faa3b2d40aada00c94040d308c0
thebinaryfelix <mateus.cfoliveira@gmail.com>
2018-04-17 20:56:14 +0200

    Added marker

---
+++ coffee-and-books/models/Bookstore.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const bookstoreSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  location: { type: { type: String }, coordinates: [Number] }
+});
+
+bookstoreSchema.index({ location: "2dsphere" });
+
+module.exports = mongoose.model("Restaurant", bookstoreSchema);
