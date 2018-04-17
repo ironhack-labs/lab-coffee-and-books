2406520bdc4883835ffc02f358bcc89a8fc4a6b2
Pedro Piury Blanquez <pedropiuryblanquez@MacBook-Air-de-Pedro.local>
2018-04-17 20:39:00 +0200

    Carga el mapa en el HTML

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   console.log('IronGenerator JS imported successfully!');
-
+  
+  function startMap() {
+    const ironhackBCN = {
+      lat: 41.3977381,
+      lng: 2.190471916};
+    const map = new google.maps.Map(
+      document.getElementById('map'),
+      {
+        zoom: 15,
+        center: ironhackBCN
+      }
+    );
+  }
+  
+  startMap();
 }, false);
