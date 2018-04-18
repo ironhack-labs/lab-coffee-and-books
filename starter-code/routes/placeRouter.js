950c5fe4a54d1e420fef9121fefee154345d91e1
Mapi Carrillo <mp.carrillo@yahoo.es>
2018-04-18 08:32:26 +0200

    Acabado CRUD

--- starter-code/routes/placeRouter.js
+++ starter-code/routes/placeRouter.js
@@ -37,5 +37,29 @@ routerPlace.get("/", (req, res) => {
         console.log(error);
       });
   });
-
+  /* CRUD -> Delete the place in DB */
+routerPlace.get("/:id/delete", (req, res) => {
+    Place.findByIdAndRemove(req.params.id)
+      .then(() => {
+        res.redirect("/");
+      })
+      .catch(error => {
+        res.render("error");
+      });
+  });
+/* CRUD -> Udpate, show place update form */
+routerPlace.get("/:id/edit", (req, res) => {
+    Place.findById(req.params.id).then(place => {
+      res.render("place_edit", { place });
+    });
+  });
+  
+  /* CRUD -> Udpate, update the place in DB */
+  routerPlace.post("/:id/edit", (req, res) => {
+    const { name, description, type, location } = req.body;
+    const updates = { name, description, type, location };
+    Place.findByIdAndUpdate(req.params.id, updates).then(() => {
+      res.redirect("/");
+    });
+  });
   module.exports = routerPlace;
