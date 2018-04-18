11378e59c3ecfdaa7511f45b27b481f58fed0ac0
Yago Vega <yagofvv@gmail.com>
2018-04-18 09:23:56 +0200

    working markers

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
+window.onload = () => {
+  const ironhackMAD = { lat: 40.392491, lng: -3.697986 };
+  // Map initialization
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: ironhackMAD
+  });
 
-  console.log('IronGenerator JS imported successfully!');
+  const myPlaces = window.places;
+  console.log(myPlaces);
 
-}, false);
+  // Add restaurant markers to map
+  let markers = [];
+  myPlaces.forEach(function(place) {
+    let title = place.name;
+    let position = {
+      lat: place.location.coordinates[0],
+      lng: place.location.coordinates[1]
+    };
+    var pin = new google.maps.Marker({
+      position: position,
+      map: map,
+      title: title
+    });
+    markers.push(pin);
+  });
+  //  var bounds = new google.maps.LatLngBounds();
+  // bounds.extend(myPlaces);
+  // map.fitBounds(bounds);
+};
