7f53c595c5da4466600a9bfc6f4ba070ebd22ed5
Pedro Piury Blanquez <pedropiuryblanquez@MacBook-Air-de-Pedro.local>
2018-04-18 09:07:27 +0200

    Post creado para que coja locations del formulario

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,18 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   console.log('IronGenerator JS imported successfully!');
-  
+
   function startMap() {
-    const ironhackBCN = {
-      lat: 41.3977381,
-      lng: 2.190471916};
+    const ironhackMAD = {
+      lat: 40.3925362,
+      lng: -3.7004556};
+      
     const map = new google.maps.Map(
       document.getElementById('map'),
       {
         zoom: 15,
-        center: ironhackBCN
+        center: ironhackMAD
       }
     );
+
+    const myMarker = new google.maps.Marker({
+      position: {
+        lat: 40.3925362,
+        lng: -3.7004556
+      },
+      map: map,
+      title: "I'm at Ironhack MADRID"
+    });
   }
   
   startMap();
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,28 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.post("/", (req, res, next) => {
+  const name = req.body.name;
+  const kind = req.body.kind;
+
+  const location = {
+    type: "Point",
+    coordinates: [ req.body.latitude, req.body.longitude ]
+  }
+
+  const newPlace = new Place({
+    name,
+    kind,
+    location
+  });
+
+  newPlace.save()
+    .then( () => {
+      res.redirect("/");
+    })
+    .catch( (err) => {
+      console.log(err);
+    });
+})
+
 module.exports = router;
