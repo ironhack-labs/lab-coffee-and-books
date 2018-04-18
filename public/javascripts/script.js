066631594ac39c9b4ee15db18db39ddcfde8d446
Nacho Lopez <ignlopezsanchez@gmail.com>
2018-04-18 09:32:47 +0200

    final antes de entregar. hace lo mismo que antes

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   console.log('IronGenerator JS imported successfully!');
+  function startMap() {
+    const ironhackBCN = {
+      lat: 40.392594,
+      lng: -3.698263};  
+    const map = new google.maps.Map(
+      document.getElementById('map'),
+      {
+        zoom: 15,
+        center: ironhackBCN
+      }
+    );
+  }
+  
+  startMap();
 
 }, false);
