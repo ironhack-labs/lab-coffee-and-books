38e34b55da2cb82072eb519ad91e4652824b7971
Mapi Carrillo <mp.carrillo@yahoo.es>
2018-04-18 08:48:52 +0200

    Conseguido mapa y marcador de una librería, marcador fijo

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -1,5 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    function startMap() {
+      const YorikBook = {
+        lat: 40.406296,
+        lng: -3.699162
+      };
+      const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: YorikBook
+      });
+      const myMarker = new google.maps.Marker({
+        position: {
+          lat: 40.406296,
+          lng: -3.699162
+        },
+        map: map,
+        title: "I'm here"
+      });
+    }
 
-  console.log('IronGenerator JS imported successfully!');
-
-}, false);
+    startMap();
+  },
+  false
+);
