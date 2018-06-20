94d55db416d612881ca0f1d0c5ea1bcae24c7bbc
John Fish <j31@users.noreply.github.com>
2018-06-20 07:40:30 +0200

    v 1 - displays map and markers

---
+++ coffee-and-books/public/javascripts/script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  console.log('script.js imported successfully!');
+
+ 
+    // const berlin = {
+    //   lat: 52.5065133, 
+    //   lng: 13.1445545
+    // };
+    
+    // const markers = []
+    
+    // const map = new google.maps.Map(document.getElementById('map'), {
+    //   zoom: 13,
+    //   center: berlin
+    // });
+  
+    // const center = {
+    //   lat: undefined,
+    //   lng: undefined
+    // }; 
+
+
+
+}, false);
