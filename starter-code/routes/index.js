5a60abbb40864b445ec13d9effeb34f0e36d4734
Abel Alonso <abel.alonso@gmail.com>
2018-07-10 19:35:59 +0200

    listing the coffee stores

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const BookStore = require('../models/bookStore');
+const CoffeeStore = require('../models/coffeeStore');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -13,6 +14,14 @@ router.get('/bookStores', (req, res, next) => {
       res.render('bookStores/list', {bookStores});
   })
   .catch(err => next())
-})
+});
+
+router.get('/coffeeStores', (req, res, next) => {
+  CoffeeStore.find()
+  .then(coffeeStores => {
+      res.render('coffeeStores/list', {coffeeStores});
+  })
+  .catch(err => next())
+});
 
 module.exports = router;
