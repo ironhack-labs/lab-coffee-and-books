5815253767cfb217f5638c1f70d2d27014514247
JuanGPC <juangpc@gmail.com>
2018-07-11 00:36:55 +0200

    advancing

--- app.js
+++ app.js
@@ -8,11 +8,14 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+// const { ensureLoggedIn, hasRole } = require('./middleware/mid');
+const flash = require("connect-flash");
 
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/starter-code', {useMongoClient: true})
+  .connect(process.env.DBURL, {useMongoClient: true})
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {
@@ -30,6 +33,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(session({
+  secret: process.env.COOKIE_SECRET,
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  }),
+  resave: true,
+  saveUninitialized: true
+}));
+app.use('./passport')(app);
+
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
@@ -38,21 +53,30 @@ app.use(require('node-sass-middleware')({
   sourceMap: true
 }));
       
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
+//default values for resposnes...
+app.use((req,res,next) => {
+  res.locals.title = 'Coffee & Books!';
+  res.locals.user = req.user;
+  res.locals.message = req.flash("error");
+  next();
+}) 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+// app.locals.title = 'Coffee & Books!';
 
 
+const users=require('.routes/users');
+app.use('/login',users);
+app.use('/signin',users);
+app.use('/logout',users);
 
 const index = require('./routes/index');
 app.use('/', index);
-
 
 module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+const passport = require('passport');
+const bcryptSalt = Number(process.env.SALTNUM);
+const bcryptSaltWord= process.env.SALTWORD;
+const router  = express.Router();
+
+router.get('/login', (req, res, next) => {
+  res.render('/users/login');
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+})
+);
+
+router.get('/signup', (req, res, next) => {
+  //logic to create isAdmin and isEditor logic
+  res.render('/users/signup');
+});
+
+router.post('/signup', (req, res, next) => {
+  // console.log(req.body)
+  const {
+    username,
+    password,
+  } = req.body;
+
+  User.findOne({
+      username
+    })
+    .then(user => {
+      if (user !== null) {
+        throw new Error("Username Already exists");
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(bcryptSaltWord.concat(password),salt);
+      
+      const newUser = new User({
+        username,
+        password: hashPass,
+        role: [req.body.role]
+      });
+
+      return newUser.save()
+    })
+    .then(user => {
+      res.redirect("/");
+    })
+    .catch(err => {
+      console.log(err);
+      res.render("auth/signup", {
+        errorMessage: err.message
+      });
+    })
+})
+
+
+
+
+
+
+
+
+module.exports = router;
+
+
