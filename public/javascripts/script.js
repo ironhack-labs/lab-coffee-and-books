d9a953d9f5082292c46730eee73adb35336ae02a
Verónica Reina <veroreinah@gmail.com>
2018-07-10 19:27:05 +0200

    Create places and show them in the map

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,42 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+    const center = {
+      lat: 40.4167,
+      lng: -3.70325
+    };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 15,
+      center: center
+    });
 
-  console.log('IronGenerator JS imported successfully!');
+    if (window.places) {
+      const bounds = new google.maps.LatLngBounds();
 
-}, false);
+      window.places.forEach(place => {
+        const icon = {
+          scaledSize: new google.maps.Size(40, 40), // scaled size
+          origin: new google.maps.Point(0, 0), // origin
+          anchor: new google.maps.Point(20, 40) // anchor
+        };
+        if (place.kind === "Café") {
+          icon.url = "/images/marker_blue.png";
+        } else {
+          icon.url = "/images/marker_orange.png";
+        }
+
+        const marker = new google.maps.Marker({
+          position: {
+            lat: place.location.coordinates[0],
+            lng: place.location.coordinates[1]
+          },
+          map: map,
+          icon: icon,
+          title: `${place.name} (${place.kind})`
+        });
+
+        bounds.extend(marker.position);
+      });
+
+      map.fitBounds(bounds);
+    }
+  }, false
+);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const Place = require("../models/Place");
+const router = express.Router();
 
-/* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  Place.find({})
+  .then(places => {
+    res.render("index", {
+      places: JSON.stringify(places)
+    });
+  })
+  .catch(err => {
+    next(err);
+  })
+});
+
+router.get("/add", (req, res, next) => {
+  res.render("places/add");
+});
+
+router.post("/add", (req, res) => {
+  const { name, kind, lat, lng } = req.body;
+
+  let location = {
+    type: 'Point',
+    coordinates: [lng, lat]
+  };
+
+  const newPlace = new Place({
+    name,
+    kind,
+    location
+  });
+
+  newPlace.save()
+    .then(place => {
+      res.redirect("/");
+    })
+    .catch(err => {
+      res.render("places/add", {
+        errorMessage: err.message
+      });
+    });
 });
 
 module.exports = router;
