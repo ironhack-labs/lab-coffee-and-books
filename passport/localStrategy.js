304ffded131f33277ebbdc2d00a7a3419c1dc87d
JuanGPC <juangpc@gmail.com>
2018-07-11 01:05:43 +0200

    passport working

--- passport/localStrategy.js
+++ passport/localStrategy.js
@@ -1,13 +1,15 @@
-const User = require('../models/User');
+const User = require('../models/user');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
+// const bcryptSalt = Number(process.env.SALTNUM);
+const bcryptSaltWord= process.env.SALTWORD;
 
 passport.use(new LocalStrategy((username, password, next) => {
     User.findOne({username})
     .then( user =>{
         if (!user) throw new Error("Incorrect Username");
-        if (!bcrypt.compareSync(password, user.password)) throw new Error("Incorrect Password");
+        if (!bcrypt.compareSync(bcryptSaltWord.concat(password), user.password)) throw new Error("Incorrect Password");
         
         return next(null, user);
     })
--- passport/serializers.js
+++ passport/serializers.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const User = require('../models/user');
 const passport = require('passport');
 
 passport.serializeUser((user, cb) => {
