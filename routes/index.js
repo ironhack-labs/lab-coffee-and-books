628cd761c2792b7206652a72071b0fa39f8b9374
“yannyyss” <yannyyss@hotmail.com>
2018-07-10 21:42:39 -0500

    terminado

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Place = require('../models/Place')
 
 /* GET home page */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.render('index');
 });
+
+router.get('/new', (req, res) => {
+  res.render('new');
+})
+
+router.post('/new', (req, res) => {
+  Place.create(req.body)
+    .then(places => {
+      res.redirect('/list')
+    }).catch(e => {
+      console.log(e)
+    })
+})
+
+router.get('/list', (req, res) => {
+  Place.find()
+    .then(places => {
+      res.render('list', { places })
+    }).catch(e => {
+      console.log(e)
+    })
+})
+
+router.get('/list/:id', (req, res) => {
+  Place.findById(req.params.id)
+  .then(place => {
+  res.render('detail', place)
+  }).catch(e => {
+    console.log(e)
+    })
+})
 
 module.exports = router;
