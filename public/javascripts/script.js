17fa4b857c439cd2797b135295912316607806a2
Patrick de la Guardia <patrickdelaguardia@Patricks-MacBook.local>
2018-07-12 08:38:19 -0500

    not done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,3 +3,74 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+function startMap() {
+
+  const ironhackBCN = {
+    lat: 41.386230,
+    lng: 2.174980
+  };
+
+  let markers = [];
+
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: ironhackBCN
+  });
+
+  let center = {
+    lat: undefined,
+    lng: undefined
+  };
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      const user_location = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      map.setCenter(user_location);
+      getPlace();
+    }, () => {
+      getPlace();
+      console.log('error in locating all things');
+    });
+  } else {
+    console.log('Browser don\'t mess with no geolocation');
+  }
+
+
+
+
+
+
+
+  function placePlaces(places) {
+    places.forEach(function (place) {
+      const center = {
+        lat: place.location.coordinates[0],
+        lng: place.location.coordinates[1]
+      };
+      const pin = new google.maps.Marker({
+        position: center,
+        map: map,
+        title: place.name
+      });
+      markers.push(pin);
+    });
+  }
+
+
+  function getPlace() {
+    axios.get("http://localhost:3000/api")
+      .then(response => {
+        console.log(response.data.places)
+        placePlaces(response.data.places);
+      })
+      .catch(error => {
+        next(error);
+      });
+  }
+}
+
+startMap();
---
+++ public/javascripts/show.js
@@ -0,0 +1,53 @@
+function startMap() {
+
+  const ironhackBCN = {
+    lat: 41.386230,
+    lng: 2.174980
+  };
+
+  let markers = [];
+
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: ironhackBCN
+  });
+
+  let center = {
+    lat: undefined,
+    lng: undefined
+  };
+
+  function placePlaces(place) {
+    // places.forEach(function(place){
+    const center = {
+      lat: place.location.coordinates[0],
+      lng: place.location.coordinates[1]
+    };
+    const pin = new google.maps.Marker({
+      position: center,
+      map: map,
+      title: place.name
+    });
+    markers.push(pin);
+    map.setCenter(center);
+    // });
+  }
+
+
+  function getPlace() {
+    axios.get("http://localhost:3000/api")
+      .then(response => {
+        res = response.data.places.find(e => {
+          return e._id === document.getElementById('id').innerText;
+        });
+        console.log(res)
+        placePlaces(res);
+      })
+      .catch(error => {
+        next(error);
+      });
+  }
+  getPlace()
+}
+
+startMap();
