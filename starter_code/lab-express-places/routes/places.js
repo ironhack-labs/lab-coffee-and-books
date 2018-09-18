babba7a70cf8a64d80479a1434e97a793db5c594
agdwm <almudenaguerraswm@gmail.com>
2018-09-18 23:22:42 +0200

    Create the functionality to create a new place in the database

--- starter_code/lab-express-places/routes/places.js
+++ starter_code/lab-express-places/routes/places.js
@@ -18,8 +18,33 @@ router.get('/', (req, res, next) => {
 		})
 });
 
+//(C)RUD -> Create Show
 router.get('/new', (req, res, next) => {
 	res.render('places/new');
+});
+
+//(C)RUD -> Create
+router.post('/new', (req, res, next) => {
+	const { name, kind, latitude, longitude } = req.body;
+
+	const newPlace = new Place({ 
+		name, 
+		kind,
+		location: {
+			type: 'Point',
+			coordinates: [Number(latitude), Number(longitude)]
+
+		}
+	});
+	
+	newPlace.save()
+		.then( place => {
+			res.redirect('/places');
+		})
+		.catch( e => {
+			console.log('Error on creating a new place', e);
+			res.render('places/new', {title: 'Create a new place'});
+		})
 });
 
 
