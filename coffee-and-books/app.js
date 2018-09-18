930b0985600cf6ca4591934d8cc71e79837cb19c
Mapno <mariorvinas@gmail.com>
2018-09-19 00:16:52 +0200

    done

--- coffee-and-books/app.js
+++ coffee-and-books/app.js
@@ -54,5 +54,7 @@ app.locals.title = 'Express - Generated with IronGenerator';
 const index = require('./routes/index');
 app.use('/', index);
 
+app.get('/',(req,res) => res.render('index'));
+
 
 module.exports = app;
--- coffee-and-books/routes/index.js
+++ coffee-and-books/routes/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router  = express.Router();
 const place = require('../models/place');
 
-/* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  place.find()
+  .then(places => res.render('index', {places, placesStr: JSON.stringify(places)}))
+  .catch(e => next(e));
 });
 
 module.exports = router;
