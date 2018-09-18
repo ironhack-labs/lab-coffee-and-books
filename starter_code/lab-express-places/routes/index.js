6358671f275da8c879ec477093472dad739359bf
agdwm <almudenaguerraswm@gmail.com>
2018-09-18 21:10:49 +0200

    CRUD Retrieve All - Show the list of places

--- starter_code/lab-express-places/routes/index.js
+++ starter_code/lab-express-places/routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router  = express.Router();
+const Place = require('../models/Place');
 
 /* GET home page */
+// router.get('/', (req, res, next) => {
+//   res.render('index');
+// });
+
+//C(R)UD -> Retrieve
 router.get('/', (req, res, next) => {
-  res.render('index');
+	Place.find()
+		.then( places => {
+			res.render('places/index', { 
+				places,
+				placesStr: JSON.stringify(places),
+				subtitle: 'Places List'
+			});
+		})
+		.catch( e => {
+			console.log('Error on retrieving the list of celebrities', e);
+			next(e);
+		})
 });
 
 module.exports = router;
