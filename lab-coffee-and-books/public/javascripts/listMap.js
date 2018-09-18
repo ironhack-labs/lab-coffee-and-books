4438cb6a4ed55324be698d6bb9aa6f1ab3fc79b0
cesararmada <cesar.armada@gmail.com>
2018-09-18 21:05:25 +0200

    done

---
+++ lab-coffee-and-books/public/javascripts/listMap.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+  });
+  geolocalize().then(center => {
+    map.setCenter(center);
+
+    place.forEach(rest => {
+      new google.maps.Marker({
+        position: {
+          lat:rest.location.coordinates[0],
+          lng:rest.location.coordinates[1]
+        },
+        map: map,
+        title: rest.name
+      });
+    })
+
+  });
+
+}, false);
