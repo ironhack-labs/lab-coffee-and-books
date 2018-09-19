55d3e546d0742b95ffcf49c44f65b044e2f82c94
Amel <amelrodr@gmail.com>
2018-09-18 22:59:44 -0500

    DE Amel Juan Carlos

---
+++ expressCoffeeBooks/models/Place.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const placeSchema = new Schema({
+
+  name: String,
+  location : {
+    type:{
+      type: String,
+      default: 'Point' 
+    },
+    address: String,
+    coordinates: [{
+      type: Number
+    }]
+  }
+
+},{
+  timestamps:{
+    updatedAt: 'updated_at',
+    createdAt: 'created_at',
+  }
+})
+
+module.exports = mongoose.model('Place', placeSchema)
