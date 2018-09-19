29537427303f625043cd30089da9e8ba2ea7d893
mish <mish@MacBook-Pro-de-mish.local>
2018-09-18 21:31:24 -0500

    terminado todo lo funcional

---
+++ lab-coffee-and-books/models/Place.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const Schema   = mongoose.Schema
+const PLM      = require('passport-local-mongoose')
+
+const placeSchema = new Schema({
+  name: String,
+  enum:['CAFE', 'BOOK'],
+  location:{
+      type:{
+        type: String,
+        default:"point"
+  },
+  address: String,
+  coordinates:[{
+    type: Number
+    }]
+  }
+},{
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+})
+
+
+module.exports = mongoose.model('Place', placeSchema)
