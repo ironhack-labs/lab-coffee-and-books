83f84962fb9d42bc606ca00fb6ab54fe281002a2
David <davidmunozvi@icloud.com>
2018-11-20 23:00:43 +0100

    Iteration#1 Create model

---
+++ starter-code/models/place.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const {Schema}   = mongoose;
+
+
+const placeSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  type: {
+    type: String,
+    required: true,
+    enum: ['coffee shop', 'bookstore'],
+  },
+  location: {
+    lat: { type: String },
+    lng: { type: String },
+  },
+});
+
+
+const Place = mongoose.model("Place", placeSchema);
+module.exports = Place;
