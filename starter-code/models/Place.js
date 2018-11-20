0d8de6ab4cb5b80b64d1489a0cb04357295e6dec
Sonia Ruiz <yune.vk@gmail.com>
2018-11-20 18:50:52 +0100

    Iteration 1 - create model

---
+++ starter-code/models/Place.js
@@ -0,0 +1,17 @@
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const placeSchema = new Schema({
+  name: String,
+  type: {
+    type: String,
+    enum: ['COFFEE SHOP', 'BOOKSTORE']
+  }
+}, {
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+});
+
+module.exports = mongoose.model('Place', placeSchema);
