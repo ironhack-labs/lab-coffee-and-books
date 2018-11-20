8a02178333106bbee71b696968f9f177df87ba86
Anna Zhuravleva <helloboysandgirls.az@gmail.com>
2018-11-20 19:59:06 +0100

    Iteration 1

---
+++ starter-code/models/place.js
@@ -0,0 +1,15 @@
+
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const placeSchema = new Schema({
+    name : { type: String},
+    type: { type: String, enum: ['coffee shop','bookstore']},
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+});
+
+const Place = mongoose.model('Place', placeSchema);
+module.exports = Place;
