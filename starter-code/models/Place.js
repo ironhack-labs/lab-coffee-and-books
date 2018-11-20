eef6eed210f42147b6be3daabc7657f4c36b55d0
Zeswen <zeswentyrael@gmail.com>
2018-11-20 18:50:36 +0100

    Iteration 2

--- starter-code/models/Place.js
+++ starter-code/models/Place.js
@@ -3,8 +3,16 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const placeSchema = new Schema({
-  name: { type: String, required: true, unique: true },
-  type: { type: String, required:true, enum:['coffee shop', 'bookstore'] },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  type: {
+    type: String,
+    required: true,
+    enum: ['coffee shop', 'bookstore'],
+  },
 });
 
 const Place = mongoose.model('Place', placeSchema);
---
+++ starter-code/routes/places.js
@@ -0,0 +1,50 @@
+const express = require('express');
+
+const placeRouter = express.Router();
+
+const Place = require('../models/Place');
+
+placeRouter.get('/', (req, res, next) => {
+  Place.find()
+    .then(places => res.render('places', { places }))
+    .catch(err => next(err));
+});
+
+placeRouter.get('/new', (req, res, next) => res.render('newPlace'));
+
+placeRouter.get('/:id', (req, res, next) => {
+  Place.findById(req.params.id)
+    .then(place => res.render('show', { place }))
+    .catch(err => next(err));
+});
+
+placeRouter.get('/:id/edit', (req, res, next) => {
+  Place.findById(req.params.id)
+    .then(place => res.render('edit', { place }))
+    .catch(err => next(err));
+});
+
+placeRouter.post('/', (req, res, next) => {
+  const myPlace = new Place({
+    name: req.body.name,
+    type: req.body.type,
+  });
+
+  myPlace.save()
+    .then(() => res.redirect('/places'))
+    .catch(() => res.redirect('/places/new'));
+});
+
+placeRouter.post('/:id', (req, res, next) => {
+  Place.findByIdAndUpdate(req.params.id, { name: req.body.name, type: req.body.type })
+    .then(() => res.redirect('/places'))
+    .catch(err => next(err));
+});
+
+placeRouter.post('/:id/delete', (req, res, next) => {
+  Place.findByIdAndRemove(req.params.id)
+    .then(() => res.redirect('/places'))
+    .catch(err => next(err));
+});
+
+module.exports = placeRouter;
