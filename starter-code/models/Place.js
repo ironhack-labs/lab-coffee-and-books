4ba861a94a3f6e2f6ff0d166c8f861396a79a571
Zeswen <zeswentyrael@gmail.com>
2018-11-20 17:51:06 +0100

    Iteration 1

---
+++ starter-code/models/Place.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const placeSchema = new Schema({
+  name: { type: String, required: true, unique: true },
+  type: { type: String, required:true, enum:['coffee shop', 'bookstore'] },
+});
+
+const Place = mongoose.model('Place', placeSchema);
+
+module.exports = Place;
