7e779f3f2f2fca7159ff0d1e799799d1e20b140d
Bianca Castro <biiianca_00@hotmail.com>
2018-11-20 19:06:28 +0100

    iteration 1

---
+++ starter-code/models/Place.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+
+
+const placeSchema = new Schema({
+    name: String,
+    type: {type: Array, enum: ["coffee shop", "bookstore"]}
+  }, {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  });
+  
+
+const Place = mongoose.model("Place", placeSchema);
+module.exports = Place;
