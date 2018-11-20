d52979fecf63d0de7921a2291dd9d4e0092a6da7
Christian Romero <christian@MacBook-Air-de-Christian.local>
2018-11-20 19:57:06 +0100

    Iteration 2

--- starter-code/models/Place.js
+++ starter-code/models/Place.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const placeSchema = new Schema({
-  name: String,
+  name: {
+    type: String,
+    required: true
+  },
   type: {
     type: String,
     enum: ["coffee shop", "bookstore"]
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,65 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Place = require('../models/Place.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  Place.find({})
+    .then(places => {
+      res.render('index', { places });
+    })
+    .catch(error => {
+      console.log("Error to find place" + error)
+      res.redirect('/')
+    })
 });
+
+router.get('/:_id', (req, res, next) => {
+  Place.findById(req.params._id)
+    .then(place => {
+      res.render('placeid', { place });
+    })
+    .catch(error => {
+      console.log("Error to find place" + error)
+      res.redirect('/')
+    })
+});
+
+router.post('/new', (req, res, next) => {
+  const genericPlace = new Place();
+  genericPlace.name = req.body.name;
+  genericPlace.type = req.body.type;
+  genericPlace.save()
+    .then(() => {
+      res.redirect('/')
+    })
+    .catch(error => {
+      console.log("Error to add a new place" + error)
+    })
+})
+
+router.post('/:_id/update', (req, res, next) => {
+  if (req.body.name === "" || req.body.type === "") {
+    res.redirect('/')
+  } else {
+    placeEdited = {}
+    placeEdited.name = req.body.name;
+    placeEdited.type = req.body.type;
+    Place.findByIdAndUpdate(req.params._id, placeEdited)
+      .then(() => {
+        res.redirect('/')
+      })
+      .catch(error => console.log("Error to update a place" + error))
+  }
+
+})
+
+router.post('/:_id/delete', (req, res, next) => {
+  Place.findByIdAndRemove(req.params._id)
+    .then(() => {
+      res.redirect('/')
+    })
+    .catch(error => console.log("Error to remove a place" + error))
+})
 
 module.exports = router;
