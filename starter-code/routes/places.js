e2c3733ae7248c00d005daa9fd9c77cc4638b951
Fer Perez Ayala <ferp@protonmail.ch>
2018-11-21 00:51:56 +0100

    Iteration 2 complete

--- starter-code/routes/places.js
+++ starter-code/routes/places.js
@@ -1,11 +1,66 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: true }));
 
 const Place = require('../models/place');
 
-/* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
-});
+router.get('/places', (req, res, next) => {
+    Place.find()
+        .then((places) => {
+            res.render('places/index', { places });
+        })
+        .catch((err) => {
+            next(err);
+        })
+})
+
+router.post('/add-place', (req, res, next) => {
+    let {name, type} = req.body;
+    let newPlace = new Place({name, type});
+
+    newPlace.save()
+    .then(()=>{
+        res.redirect('/places');
+    })
+    .catch((err)=>{
+        next(err);
+    })
+})
+
+router.post('/delete-place/:id', (req, res, next) => {
+    Place.findByIdAndRemove(req.params.id)
+    .then(()=>{
+        res.redirect('/places');
+    })
+    .catch((err)=>{
+        next(err);
+    })
+})
+
+router.get('/places/edit/:id', (req, res, next) => {
+    Place.findById(req.params.id)
+    .then((place)=>{
+        res.render('places/edit', place);
+    })
+    .catch((err)=>{
+        next(err);
+    })
+    
+})
+
+router.post('/edit-place/:id', (req, res, next)=>{
+
+    let {name, type} = req.body;
+
+    Place.findByIdAndUpdate(req.params.id, { name, type })
+    .then(()=>{
+        res.redirect('/places');
+    })
+    .catch((err)=>{
+        next(err);
+    })
+})
 
 module.exports = router;
