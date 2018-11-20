be7e5b733afda54439e15a58d11c4264e413d981
sitobusnan <sito@citytonight.es>
2018-11-20 21:22:41 +0100

    it 3 con mapa sin puntos

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -1,5 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
+window.onload = function(){
+  startMap();
+}
+// document.addEventListener('DOMContentLoaded', () => {
 
-  console.log('IronGenerator JS imported successfully!');
+//   console.log('IronGenerator JS imported successfully!');
 
-}, false);
+// }, false);
+
+
+// function randomIntFromInterval(min, max) // min and max included
+// {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+
+const mapDOMElement = document.getElementById('map')
+// window.chosenLocation = null
+
+function startMap() {
+    const ironhackBCN = {
+        lat: 42.4592723,
+        lng: -6.0946061};
+    const map = new google.maps.Map(
+      document.getElementById('map'),
+      {
+        zoom: 12,
+        center: ironhackBCN
+      }
+    );
+  }
