3bb2819c26b742d47d01d6ea0f2dc8a3b0b58f83
Rodrigo.Munoz <rodrigomunozga@gmail.com>
2018-11-21 09:14:14 +0100

    Iteration 3

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -3,3 +3,27 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+function startMap() {
+  const ironhackBCN = {
+  	lat: 41.3977381,
+  	lng: 2.190471916};
+  const map = new google.maps.Map(
+    document.getElementById('map'),
+    {
+      zoom: 5,
+      center: ironhackBCN
+    }
+  );
+}
+
+startMap();
+
+const myMarker = new google.maps.Marker({
+  position:{
+    lat: 41.3977381,
+    lng: 2.190471916
+  },
+  map: map,
+  title: 'I am here'
+});
