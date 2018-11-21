d106aa406941b4f45ac8660b14db298083bb53be
PomPom248 <sofiroggero@gmail.com>
2018-11-21 09:36:40 +0100

    iteration 3 - unfinished

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -1,14 +1,9 @@
+function initMap() {
 
+  var coordinates = { lat: 0, lng: 0 };
+  var map = new google.maps.Map(
+    document.getElementById('map'), { zoom: 4, center: coordinates });
+  var marker = new google.maps.Marker({ position: coordinates, map: map });
+}
 
-function startMap() {
-  const coordinates = {
-    lat: 0,
-    lng: 0,
-  };
-
-  const map = new google.maps.Map(
-    document.getElementById('map'), {
-      zoom: 10,
-      center: coordinates,
-    },
-  );
+initMap()
