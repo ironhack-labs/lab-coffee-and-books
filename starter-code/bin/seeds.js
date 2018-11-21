9645daa946f66aea8855c97efa7318d11c785eef
Jaime <herrerotajaime@gmail.com>
2018-11-21 09:34:05 +0100

    iteracion 2

---
+++ starter-code/bin/seeds.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const Place = require('../models/Place.js');
+ mongoose.connect(`mongodb://localhost/${process.env.DB}`);
+ const places = [
+  {
+    name: "Place1",
+    type: "coffee shop",
+  },
+  {
+    name: "Place2",
+    type: "bookstore",
+  },
+  {
+    name: "Place3",
+    type: "coffee shop",
+  }
+]
+ Place.create(places, (err) => {
+  if (err) { throw (err) }
+  console.log(`Created ${places.length} places`)
+  mongoose.connection.close()
+}); 
