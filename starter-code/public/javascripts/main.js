9878772ad2875311fbcd9562f1cd8300a129bed6
Ferran <ferranlao@hotmail.com>
2018-11-23 23:52:50 +0100

    all map added

--- starter-code/public/javascripts/main.js
+++ starter-code/public/javascripts/main.js
@@ -1,22 +1,56 @@
-
 function startMap() {
-  const ironhackBCN = {
-    lat: Number(document.querySelector("input[name=latitude]").value),
-    lng: Number(document.querySelector("input[name=longitude]").value)
-  };
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 9,
-    center: ironhackBCN
-  });
-  const myMarker = new google.maps.Marker({
-    position: {
+  if (document.querySelector("input[name=latitude]")) {
+    const center = {
       lat: Number(document.querySelector("input[name=latitude]").value),
       lng: Number(document.querySelector("input[name=longitude]").value)
-    },
-    
-    map: map,
-    title: "I'm here"
-  });
+    };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 9,
+      center: center
+    });
+    const myMarker = new google.maps.Marker({
+      position: {
+        lat: Number(document.querySelector("input[name=latitude]").value),
+        lng: Number(document.querySelector("input[name=longitude]").value)
+      },
+
+      map: map,
+      title: "I'm here"
+    });
+  } else {
+    var coord = document.querySelectorAll(".pos"); 
+
+    const center = {
+      lat: 40.3925321,
+      lng: -3.6982669
+    };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 7,
+      center: center
+    });
+    coord.forEach(e => { 
+        let split= e.innerText.split(",")
+        console.log(split)
+        const myMarker = new google.maps.Marker({
+            position: {
+              lat:Number(split[1]),
+              lng: Number(split[0])
+            },
+      
+            map: map,
+            title: "I'm here"
+          });
+    });
+    const myMarker = new google.maps.Marker({
+      position: {
+        lat: Number(document.querySelector("input[name=latitude]").value),
+        lng: Number(document.querySelector("input[name=longitude]").value)
+      },
+
+      map: map,
+      title: "I'm here"
+    });
+  }
 }
 
 startMap();
