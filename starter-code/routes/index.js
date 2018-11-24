417dedc377fc5a55eb40eec10c3905285428f433
sandrarp <sandra.rueda.pr@gmail.com>
2018-11-24 10:22:56 +0100

    Partially done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router  = express.Router();
+const Place = require('../models/place')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  Place.find().then(places => {
+    
+    const allplaces = places.map(place => {
+      return {
+        _id: place._id,
+        name: place.name,
+        type: place.type
+      }
+    })
+
+    console.log(allplaces);
+
+    res.render('index',{
+      places: allplaces
+    });
+  })
 });
 
 module.exports = router;
