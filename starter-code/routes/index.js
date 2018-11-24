4d5d1ae6e9f21bd21cf71340b8ba82155746d061
Leticia Olábarri <leticiaolabarri@yahoo.es>
2018-11-24 09:06:44 +0100

    done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,119 @@
 const express = require('express');
 const router  = express.Router();
+const Place = require('../models/place');
 
-/* GET home page */
+// GET => render the form to create a new place
+router.get('/new', (req, res, next) => {
+  res.render('places/new');
+});
+
+// POST => to create new place and save it to the DB
+router.post('/', (req, res, next) => {
+  let location = {
+    type: 'Point',
+    coordinates: [req.body.longitude, req.body.latitude]
+  };
+  
+	const newPlace = new Place({
+		name: req.body.name,
+		description: req.body.description,
+		location: location
+	});
+
+	newPlace.save((error) => {
+		if (error) { 
+			next(error); 
+		} else { 
+			res.redirect('/places');
+		}
+	});
+});
+
+// GET => to retrieve all the places from the DB
 router.get('/', (req, res, next) => {
-  res.render('index');
+	Place.find({},(error, placesFromDB) => {
+		if (error) { 
+			next(error); 
+		} else { 
+			res.render('places/index', { restaurants: placesFromDB });
+		}
+	});
+});
+
+// GET => get the form pre-filled with the details of one place
+router.get('/:places_id/edit', (req, res, next) => {
+	Place.findById(req.params.place_id, (error, place) => {
+		if (error) {
+			next(error);
+		} else {
+			res.render('places/update', { place });
+		}
+	});
+});
+
+// POST => save updates in the database
+router.post('/:place_id', (req, res, next) => {
+	Place.findById(req.params.place_id, (error, place) => {
+		if (error) { 
+      next(error); 
+    } else {
+			place.name        = req.body.name;
+			place.description = req.body.description;
+			place.save(error => {
+				if (error) { 
+					next(error); 
+				} else { 
+					res.redirect(`/places/${req.params.place_id}`); 
+				}
+			});
+		}
+	});
+});
+
+// DELETE => remove the place from the DB
+router.get('/:place_id/delete', (req, res, next) => {
+	Place.remove({ _id: req.params.place_id }, function(error, place) {
+		if (error) {
+			next(error);
+		} else {
+			res.redirect('/places');
+		}
+	});
+});
+
+
+// to see raw data in your browser, just go on: http://localhost:3000/api
+router.get('/api', (req, res, next) => {
+	Place.find({}, (error, allPlacesFromDB) => {
+		if (error) { 
+			next(error); 
+		} else { 
+			res.status(200).json({ places: allPlacesFromDB });
+		}
+	});
+});
+
+// to see raw data in your browser, just go on: http://localhost:3000/api/someIdHere
+router.get('/api/:id', (req, res, next) => {
+	let placeId = req.params.id;
+	Place.findOne({_id: placeId}, (error, onePlaceFromDB) => {
+		if (error) { 
+			next(error) 
+		} else { 
+			res.status(200).json({ place: onePlaceFromDB }); 
+		}
+	});
+});
+
+// GET => get the details of one restaurant
+router.get('/:place_id', (req, res, next) => {
+	Place.findById(req.params.place_id, (error, place) => {
+		if (error) {
+			next(error);
+		} else {
+			res.render('places/show', { place: place });
+		}
+	});
 });
 
 module.exports = router;
