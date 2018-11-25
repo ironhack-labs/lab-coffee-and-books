976a6b9cb4b9be19f8514be6998c640f3c14d51b
aemartos <aemartos@gmail.com>
2018-11-25 12:26:48 +0100

    Join addMarker and addWindow functions

--- starter-code/public/javascripts/main.js
+++ starter-code/public/javascripts/main.js
@@ -1,11 +1,6 @@
 const loadData = (map) => {
   places.forEach(p => {
     let color = p.type === "coffee shop" ? 'red' : 'blue';
-    let marker = addMarker(p.name, {lat: p.location.coordinates[0], lng: p.location.coordinates[1]}, map, color);
-    let content = `<h1>${p.name}</h1><p>${p.description}</p>`;
-    let infowindow = new google.maps.InfoWindow({content, maxWidth: 200});
-    marker.addListener('click', function() {
-      infowindow.open(map, marker);
-    });
-})
+    addMarker(p.name, p.description, {lat: p.location.coordinates[0], lng: p.location.coordinates[1]}, map, color);
+  })
 }
