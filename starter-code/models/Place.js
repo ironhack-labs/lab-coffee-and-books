ec6b7557f6b2d792ff26ea385d4a8a3b6932e650
MontseOrtiz <montse_2908@hotmail.com>
2018-11-27 15:11:44 -0600

    Detalles, Update

---
+++ starter-code/models/Place.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const placeSchema = new Schema(
+  {
+    name: String,
+    tipo: {
+      type: String,
+      enum: ["coffee shop", "bookstore"]
+    },
+    location: {
+      type: {
+        type: String,
+        default: "Point"
+      },
+      coordinates: [Number]
+    }
+  },
+  {
+    timestamps: {
+      createdAt: true,
+      updatedAt: true
+    }
+  }
+);
+
+module.exports = mongoose.model("Place", placeSchema);
---
+++ starter-code/routes/places.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const router = express.Router();
+const Place = require("../models/Place");
+
+//CREATE
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
+
+router.post("/new", (req, res, next) => {
+  const r = {
+    name: req.body.name,
+    tipo: req.body.tipo,
+    location: {
+      type: "Point",
+      coordinates: [req.body.lng, req.body.lat]
+    }
+  };
+  Place.create(r)
+    .then(places => {
+      res.redirect("/places/" + places._id);
+    })
+    .catch(e => next(e));
+});
+
+//UPDATE
+router.get("/update/:id", (req, res, next) => {
+  const id = req.params.id;
+  Place.findById(id)
+    .then(places => {
+      res.render(`places/update`, places);
+    })
+    .catch(e => next(e));
+});
+
+router.post("/update/:id", (req, res, next) => {
+  const id = req.params.id;
+  let { name, tipo, lat, lng } = req.body;
+  Place.findByIdAndUpdate(id, { name, tipo, lat, lng }, { new: true }, null)
+    .then(places => {
+      res.redirect(`/places/${id}`);
+    })
+    .catch(error => {
+      res.render("places/new", { places: req.body, error });
+    });
+});
+
+//DELETE
+router.get("/delete/:id", (req, res, next) => {
+  const id = req.body.id;
+  Place.findByIdAndDelete(id)
+    .then(places => {
+      res.redirect("/places");
+    })
+    .catch(e => next(e));
+});
+
+//DISPLAY
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  Place.findById(id).then(places => {
+    res.render("places/detail", places);
+  });
+});
+
+router.get("/", (req, res, next) => {
+  Place.find()
+    .then(places => {
+      res.render("places/list", { places: places });
+    })
+    .catch(e => next(e));
+});
+
+module.exports = router;
