4d09f44fd35728fc1670e72a71abf09f0f87105a
Gabriel Pacheco <pacheco.sga@gmail.com>
2018-11-27 20:37:11 -0600

    MEX - PP - Sabas, Miguel y Gabriel

--- starter-code/routes/places.js
+++ starter-code/routes/places.js
@@ -2,12 +2,60 @@ const express = require('express');
 const router  = express.Router();
 const Place = require('../models/Place')
 
-router.get('/list/:id',(req,res,next)=>{
-    console.log(req.params)
-    const {id} = req.params.id
+router.post('/modify/save', (req,res,next)=>{
+    //res.send(req.body)
+    Place.updateOne({'_id':req.body.id},{'name':req.body.name,
+                                         'tipo':req.body.tipo,
+                                         'coordinates':[req.body.lat,req.body.lng]})
+                                        .then(result =>{
+                                            res.send(result)
+                                            //res.redirect('/detail/'+req.body.id)
+                                        })
+})
+
+router.get('/modify/:id', (req,res,next)=>{
+    Place.findById(req.params.id)
+    .then(place=>{
+        //res.send(place.name)
+        const lat = place.coordinates[0]
+        const lng = place.coordinates[1]
+
+        const placeOK = {
+            id:place.id,
+            name: place.name,
+            tipo: place.tipo,
+            lat:lat,
+            lng:lng
+        }
+        res.render('places/modify',placeOK)
+})
+})
+
+router.get('/detail/:id',(req,res,next)=>{
+    //res.send(req.params.id)
+    const id = req.params.id
     Place.findById(id)
     .then(place=>{
-        res.render('places/detail',place)
+        //res.send(place.tipo)
+        const lat = place.coordinates[0]
+        const lng = place.coordinates[1]
+
+        const placeOK = {
+            id:place.id,
+            name: place.name,
+            tipo: place.tipo,
+            lat:lat,
+            lng:lng
+        }
+        res.render('places/detail',placeOK)
+    })
+})
+
+router.get('/delete/:id', (req,res, next)=>{
+    const id = req.params.id
+    Place.deleteOne({'_id':id})
+    .then(result=>{
+        res.redirect('/places/list')
     })
 })
 
