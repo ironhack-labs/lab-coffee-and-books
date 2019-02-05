bea07be9ff9c22ab247ce741d3cf936f4cff7aa6
Zenchy <alvaro.corral.soilan@gmail.com>
2019-02-05 22:36:23 +0100

    A punto de pintar el mapa

--- starter-code/models/place.js
+++ starter-code/models/place.js
@@ -4,8 +4,14 @@ const Schema=mongoose.Schema
 const restaurantSchema=new Schema({
 
   name:String,
-  type:String, enum :[coffee, shop, bookstore],
-  timestamps: { createdAt: 'created_at',updatedAt:'updated_at' }
-})
+  type:String, enum :['coffeeshop', 'bookstore'],
+  location:{type:{type:String},coordinates:[Number]}}, 
+  {
+    timestamps: true
+  })
 
-restaurantSchema.index(location, '2dsphere')
+  const Restaurant=mongoose.model("restaurant",restaurantSchema)
+
+
+restaurantSchema.index({location: '2dsphere'})
+module.exports=Restaurant
--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -3,3 +3,18 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+// function startMap() {
+//   const ironhackBCN = {
+//   	lat: 41.3977381,
+//   	lng: 2.190471916};
+//   const map = new google.maps.Map(
+//     document.getElementById('map'),
+//     {
+//       zoom: 5,
+//       center: ironhackBCN
+//     }
+//   );
+// }
+
+// startMap();
