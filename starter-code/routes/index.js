39da310e8a8f22d450d9c4a01ccc729f150a34b9
Sofía <sofianombela@gmail.com>
2019-02-05 19:58:50 +0100

    iteration 1: create model

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const router  = express.Router();
 
+
+const Place = require("../models/place");
+
 /* GET home page */
-router.get('/', (req, res, next) => {
+router.get('/place', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/place/new', (req, res, next) => {
+  res.render('new');
+});
+
+router.post('/place', (req, res, next) => {
+  let location = {
+    type: 'Point',
+    coordinates: [req.body.longitude, req.body.latitude]
+    };
+    
+    const newPlace = new Place({
+      name:        req.body.name,
+      type:        req.body.value,
+      location:    location
+    });
+  
+    newPlace.save((error) => {
+      if (error) { 
+        next(error); 
+      } else { 
+        console.log("Error al salvar")
+        res.redirect('/place');
+      }
+    })
 });
 
 module.exports = router;
