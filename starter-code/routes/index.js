b9c465f9025e0de5bc11ed486f0ff70d05c84c17
Ruben Villar <ruben@rubenvillar.net>
2019-02-05 20:52:55 +0100

    second iteration

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,50 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Place = require("../models/place");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  Place.find()
+    .then(places => res.render("index", { places }))
+    .catch(error => console.log(error));
+});
+
+router.post("/add", (req, res, next) => {
+  const { name, type, latitude, longitude } = req.body;
+  const location = {
+    lat: latitude,
+    lon: longitude
+  };
+  const newPlace = new Place({ name, type, location });
+  newPlace
+    .save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch(error => console.log(error));
+});
+
+router.post("/edit/:id", (req, res, next) => {
+  Place.findById(req.params.id)
+    .then(place => res.render("edit", { place }))
+    .catch(error => error);
+});
+
+router.post("/delete/:id", (req, res, next) => {
+  Place.findByIdAndRemove(req.params.id)
+    .then(() => res.redirect("/"))
+    .catch(error => error);
+});
+
+router.post("/:id/edit", (req, res, next) => {
+  const { name, place, latitude, longitude } = req.body;
+  const location = {
+    lat: latitude,
+    lon: longitude
+  };
+  Place.findByIdAndUpdate(req.params.id, { name, place, location })
+    .then(() => res.redirect("/"))
+    .catch(error => console.log(error));
 });
 
 module.exports = router;
