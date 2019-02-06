e0a6a9c91de196a8ead2914f0487bb07b7879839
Mr.X <seniorito.x@icloud.com>
2019-02-06 11:01:19 +0100

    added geolaction

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -3,6 +3,27 @@ document.addEventListener(
   () => {
     console.log("IronGenerator JS imported successfully!");
     
+    if (navigator.geolocation) {
+
+      // Get current position
+      // The permissions dialog will pop up
+      navigator.geolocation.getCurrentPosition(function (position) {
+        // Create an object to match Google's Lat-Lng object format
+        const center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        console.log('center: ', center)
+        // User granted permission
+        // Center the map in the position we got
+      }, function () {
+        // If something goes wrong
+        console.log('Error in the geolocation service.');
+      });
+    } else {
+      // Browser says: Nah! I do not support this.
+      console.log('Browser does not support geolocation.');
+    }
 
   
     function startMap() {
