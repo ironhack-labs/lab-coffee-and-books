cb50f44e2f3e5a61539566296ba81e055dd05a7c
Nira <niralonso@gmail.com>
2019-02-06 15:32:16 +0100

    .

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,65 @@
 const express = require('express');
 const router  = express.Router();
+const Place = require('../models/place');
+
+
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/place', (req, res, next) => {
+  res.render('place');
+});
+
+router.get('/place/new', (req, res, next) => {
+  res.render('new');
+});
+
+router.post('/place/new', (req, res, next)=>{
+  let location = {
+    type: 'Point',
+    coordinates: [req.body.longitude, req.body.latitude]
+    };
+    
+    const newPlace = new Place({
+    name:        req.body.name,
+    type:        req.body.value,
+    location:    location
+    });
+  
+    newPlace.save((error) => {
+    if (error) { 
+      next(error); 
+    } else { 
+      res.redirect('/place');
+    }
+    })
+})
+
 module.exports = router;
+
+
+
+
+// AAIzaSyABf38LrWz2AS35kKqlL1veYrhQTQGrtsIIzaSyABf38LrWz2AS35kKql
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
