94cf2f6c03cc0e9e1827d56eb3552a7da0990e94
Mr.X <seniorito.x@icloud.com>
2019-02-06 09:56:24 +0100

    Me está dando fallos el post de new Place😢

--- starter-code/routes/place.js
+++ starter-code/routes/place.js
@@ -14,4 +14,11 @@ router.get('/newPlace', (req, res, next) => {
   res.render('place/newPlace')
 })
 
+router.post('/newPlace', (req, res, next) => {
+  place = ({name:req.body.name, type:req.body.type, location:{lat:+req.body.lat, lng:+req.body.lng}}) 
+  .then(place => res.json)
+  // .then(place => place.create, res.redirect('/place/index'))
+  .catch(err => console.log(err))
+})
+
 module.exports = router;
