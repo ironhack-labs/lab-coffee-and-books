eda80fa65470f8b891481d19d6366d665047462b
j0nl1 <jonlitech@gmail.com>
2019-02-06 10:37:30 +0100

    Removed Axio Dependency

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -10,7 +10,7 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 40.4381311, lng: -3.8196222 },
     zoom: 8
   });
 }
@@ -18,18 +18,20 @@ function initMap() {
 initMap();
 
 function showMarkers() {
-  axios.get(`http://localhost:3000/map`).then(places => {
-    places.data.forEach(place => {
-      new google.maps.Marker({
-        map: map,
-        position: {
-          lat: place.location.coords.lat,
-          lng: place.location.coords.lng
-        },
-        title: place.name
+  fetch("http://localhost:3000/map")
+    .then(response => response.json())
+    .then(places => {
+      places.forEach(place => {
+        new google.maps.Marker({
+          map: map,
+          position: {
+            lat: place.location.coords.lat,
+            lng: place.location.coords.lng
+          },
+          title: place.name
+        });
       });
     });
-  });
 }
 
 showMarkers();
