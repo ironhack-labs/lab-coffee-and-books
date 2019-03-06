a7dcfd2015998e59dbb257b1cc773e7ee3b250b1
Daniel Herrero Hernando <danielherrerohernando@gmail.com>
2019-03-06 16:57:18 +0100

    done

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -1,5 +1,85 @@
+
 document.addEventListener('DOMContentLoaded', () => {
 
-  console.log('IronGenerator JS imported successfully!');
-
-}, false);
+  
+  const ironhackMad = {
+      lat: 40.392757,
+      lng: -3.698256
+    }
+  
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center:  ironhackMad
+  })
+  
+    startMap = () => {
+  
+      map.addListener("click", function(e) {
+        document.getElementById("latitude").value = e.latLng.lat();
+        document.getElementById("longitude").value = e.latLng.lng();
+      });
+  
+    const myMarker = new google.maps.Marker({
+      position: ironhackMad,
+      map: map,
+      title: 'IronHack'
+    })
+  
+  
+    if (navigator.geolocation) {
+  
+      navigator.geolocation.getCurrentPosition((position) => {
+  
+        const myPosition = {
+          lat: position.coords.latitude,
+          lng:  position.coords.longitude
+        }
+  
+        const myMarkerPosition = new google.maps.Marker({
+          position: myPosition,
+          map: map,
+          title: "Aquí estoy yo!"
+        })
+  
+        console.log("posición", myPosition)
+  
+      }, () => {
+        console.log("Error en la geolocalización")
+      })
+  
+    } else {
+      console.log("Comprate un pc")
+    }
+  
+  
+    }
+  
+  
+  
+    placePlaces = (places) => {
+  
+      const markers = []
+  
+      places.forEach(place => {
+        const center = {
+          lat: place.location.coordinates[1],
+          lng: place.location.coordinates[0]
+        }
+  
+        const marker = new google.maps.Marker({
+          position: center,
+          map: map,
+          title: place.name
+        })
+        markers.push(marker)
+      })
+    }
+  
+    startMap()
+    placePlaces(window.result)
+  
+  
+  
+    console.log('IronGenerator JS imported successfully!');
+  
+  }, false);
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const router  = express.Router();
+const Place = require('../models/place')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+
+  Place.find()
+  .then(places => {
+    res.render('index', {result: JSON.stringify(places)});
+  })
+  .catch(err => {
+    console.log(err)
+  })
+  
 });
+
+router.post('/places', (req, res, next) => {
+
+  const {name, description, latitude, longitude} = req.body
+
+  let location = {
+    type: "Point",
+    coordinates: [longitude, latitude]
+  }
+
+  const newPlace = new Place({
+    name,
+    description,
+    location
+  })
+
+  newPlace.save(err => {
+    if (err) {
+      next(err)
+    } else {
+      res.redirect('/')
+    }
+  })
+
+})
 
 module.exports = router;
