7e4fb06801da2eb90aa89a009eee727932bc40ea
Dan Mihai Grigoriciuc <griganu06@gmail.com>
2019-03-07 09:17:49 +0100

    Sin finalizar

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -1,5 +1,54 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  console.log('IronGenerator JS imported successfully!');
+  const coffeeShop = {
+    lat: 40.420651,
+    lng: -3.705942,
+  }
+  
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center:  coffeeShop
+  })
 
+  startMap = () => {
+
+  map.addListener("click", function(e) {
+    document.getElementById("latitude").value = e.latLng.lat();
+    document.getElementById("longitude").value = e.latLng.lng();
+  });
+
+  
+  const myMarker = new google.maps.Marker({
+    position: coffeeShop,
+    map: map,
+    title: 'Cafeteria'
+  })
+}
+    placesCoffeesBooks = (places) => {
+
+    const markers = []
+
+    
+
+    places.forEach(place => {
+    
+      const center = {
+        lat: place.location.coordinates[1],
+        lng: place.location.coordinates[0]
+      }
+
+      const marker = new google.maps.Marker({
+        position: center,
+        map: map,
+        title: place.name
+      })
+      markers.push(marker)
+    })
+  }
+
+  
+  startMap()
+  placesCoffeesBooks(window.result)
+  
 }, false);
+
