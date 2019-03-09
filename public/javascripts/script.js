b69f94ae78931636f5087063c79843e9c03c17f7
Marco Santonastasi <marco.santonastasi@gmail.com>
2019-03-09 17:11:40 +0100

    action on modal dialog

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,7 @@
 document
   .addEventListener(
     'DOMContentLoaded',
-    async () =>
+    () =>
       {
         console.log('IronGenerator JS imported successfully!')
 
@@ -11,17 +11,8 @@ document
 
         new google.maps.Marker({position: naples, map: map})
         
-        const serverResponse = await fetch("http://localhost:3000/api/places")
-        const jsonData = await serverResponse.json()
-        jsonData
-          .forEach(
-            place =>
-            {
-              var coords = place.location.coordinates;
-              var latLng = new google.maps.LatLng(coords[1],coords[0])
-              new google.maps.Marker({position: latLng, map: map})
-            }
-          )
+        //map.data.loadGeoJson('/api/places/5c813df2ec98871b2694b32c') // IF the endpoint was a pure geoJSON emitter we couls use the built-in function
+        gMapUpdate(map, "http://localhost:3000/api/places")
       },
     false
   )
@@ -38,5 +29,14 @@ document
           new google.maps.Marker({position: latLng, map: map})
         }
       )
-
   }
+
+  $('#actionModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var action = button.data('action') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('.modal-title').text(action + button)
+    modal.find('.modal-body input').val("VALUE")
+  })
