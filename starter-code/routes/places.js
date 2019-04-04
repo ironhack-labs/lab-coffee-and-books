57f7e74e3751ec6ee31b57975e2949a0b30a44c8
Tomas Freire Knight <tomasfreire@gmail.com>
2019-04-03 22:21:56 -0600

    iterations 1 and 2 done

---
+++ starter-code/routes/places.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+const Place = require('../models/places');
+
+
+router.get('/index', (req, res) => {
+  Place.find()
+  .then(places => {
+    res.render('places/index', {places: places})
+  })
+  .catch(err => {
+    console.log(`Error showing the places in the DB: ${err}`)
+  })
+})
+
+
+router.get('/new', (req, res) => {
+  res.render('places/new');
+})
+
+router.post('/new', (req, res) => {
+  Place.create(req.body)
+    .then(() => {
+      res.redirect('/places/index');
+    })
+    .catch(err => {
+      console.log(`Error creting the place: ${err}`)
+    })
+});
+
+router.get('/:id/edit', (req, res) => {
+  const {id} = req.params;
+  Place.findById(id)
+    .then(place => {
+      res.render('places/edit', place)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+router.post('/:id/delete', (req, res) => {
+  const {id} = req.params;
+  Place.findByIdAndRemove(id)
+    .then(() => {
+      res.redirect('/places/index')
+    })
+    .catch(err => {
+      console.log(`There was an error deleting the place: ${err}`)
+    })
+})
+
+router.post('/:id', (req, res) => {
+  const {id} = req.params;
+  Place.findByIdAndUpdate(id, {$set: {...req.body}})
+    .then(() => {
+      res.redirect('/places/index')
+    })
+    .catch(err => {
+      console.log(`There was an error editing the place: ${err}`)
+    })
+})
+
+module.exports = router;
