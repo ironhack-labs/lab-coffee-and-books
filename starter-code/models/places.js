1010151258215b4bae6e4976b6f6e697e33bcf1e
lockeas16 <lockeas16@gmail.com>
2019-04-03 22:46:07 -0600

    probando merge, solo se agrega location al model

--- starter-code/models/places.js
+++ starter-code/models/places.js
@@ -1,24 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const placeSchema = new Schema({
-  name: String,
-  type: {
-    type: String,
-    enum: ['Coffee Shop', 'Bookstore']
+const placeSchema = new Schema(
+  {
+    name: String,
+    type: {
+      type: String,
+      enum: ["Coffee Shop", "Bookstore"]
+    },
+
+    location: {
+      type: {
+        type: String,
+        default: "Point"
+      },
+      coordinates: [Number]
+    }
+  },
+  {
+    timestamps: true
   }
-  // },
-  // location: {
-  //   type: {
-  //     type: String,
-  //     default: 'Point'
-  //   },
-  //   coordinates: [Number]
-  // }
-}, {
-  timestamps: true
-})
+);
 
-placeSchema.index({location: '2dsphere'});
+placeSchema.index({ location: "2dsphere" });
 
-module.exports = mongoose.model('Place', placeSchema);
+module.exports = mongoose.model("Place", placeSchema);
