f38fa3c2688fad3a42d752cbfd19e4603ec4364f
oscarmtzo <icehman@hotmail.com>
2019-07-02 16:25:40 -0500

    PP-FT-MEX Oscar Martinez

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router  = express.Router();
-
+const  { 
+  getPlaces, 
+  getCreatePlace, 
+  postCreatePlace, 
+  getOnePlace, 
+  getEditPlace, 
+  postEditPlace, 
+  getDeletePlace} = require('../controllers/index.controllers')
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
-});
+
+//******************* cRud *************************+ */
+router.get('/places/create', getCreatePlace)
+router.post('/places/create', postCreatePlace)
+//******************* CRUD *************************+ */
+router.get('/', getPlaces)
+router.get('/places/:id', getOnePlace)
+//******************* CRUD *************************+ */
+router.get('/places/:id/edit', getEditPlace)
+router.post('/places/:id/edit', postEditPlace)
+//******************* CRUD *************************+ */
+router.get('/places/:id/delete', getDeletePlace)
 
 module.exports = router;
