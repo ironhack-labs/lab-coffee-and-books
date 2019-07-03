44fc8df4e4f57bb69a4b0d7a08b310c97e99b79b
Lain <kino_neko_kira@hotmail.com>
2019-07-03 11:11:47 -0500

    Pair Hecho en Clase

---
+++ starter-code/controllers/index.controllers.js
@@ -0,0 +1,39 @@
+const Place = require('../model/Place')
+
+exports.getPlaces = (req, res, next) => {
+  Place.find()
+  .then(places => res.render('index', {places}))
+  .catch(err => res.send(err))
+}
+
+exports.getOnePlace = (req, res, next) => {
+  Place.findById(req.params.id)
+  .then(place => res.render('place', place))
+  .catch(err => next(err))
+}
+
+exports.getCreatePlace = (req, res, next) => res.render('create')
+
+exports.postCreatePlace = (req, res, next) => {
+  Place.create({...req.body})
+  .then(place => res.redirect(`/places/${place._id}`))
+  .catch(err => next(err))
+}
+
+exports.getEditPlace = (req, res, next) => {
+  Place.findById(req.params.id)
+  .then(place => res.render('edit', place))
+  .catch(err => next(err))
+}
+
+exports.PostEditPlace = (req, res, next) => {
+  Place.findByIdAndUpdate(req.params.id, {...req.body}, {new: true})
+  .then(place => res.redirect(`/places/${place._id}`))
+  .catch(err => next(err))
+}
+
+exports.getDeletePlace = (req, res, next) => {
+  Place.findByIdAndDelete(req.params.id)
+  .then(() => res.redirect('/'))
+  .catch(err => next(err))
+}
