2661d24ec89e7ad9367da0c167333d7b6068b969
Uira Oliveira <uira.sampaio@hotmail.com>
2019-07-09 22:43:04 -0300

    done

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -1,5 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
-
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+const markers = [];
+
+const ironhackSP = {
+  lat: -23.5617375,
+  lng: -46.6601331
+};
+
+const map = new google.maps.Map(
+  document.getElementById('map'),
+  {
+    zoom: 10,
+    center: ironhackSP
+  }
+);
+
+const getShops = () => {
+  axios.get('/api')
+    .then((response) => {
+      placeShops(response.data.shops);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+}
+
+const placeShops = (shops) => {
+  shops.forEach(function (shop) {
+    const center = {
+      lat: shop.location.coordinates[1],
+      lng: shop.location.coordinates[0]
+    };
+    const pin = new google.maps.Marker({
+      position: center,
+      map: map,
+      title: shop.name
+    });
+    markers.push(pin);
+  });
+}
+
+getShops();
---
+++ starter-code/routes/places.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const router = express.Router();
+const Places = require('../models/place');
+
+router.get('/create', (req, res, next) => {
+  res.render('./places/add');
+});
+
+router.post('/create', (req, res, next) => {
+  const { name, type, latitude, longitude } = req.body;
+
+  const location = {
+    type: 'Point',
+    coordinates: [parseFloat(longitude), parseFloat(latitude)]
+  }
+
+
+  const newPlace = new Places({
+    name,
+    type,
+    location,
+  });
+
+  newPlace.save()
+    .then((item) => {
+      console.log(`${item} created`)
+      res.redirect('/display');
+    })
+    .catch(err => console.log(`${err} fire! Fire!`));
+});
+
+
+router.get('/api', (req, res, next) => {
+  Places.find()
+    .then((shops) => {
+      res.status(200).json({ shops });
+    })
+    .catch(error => console.log(error));
+});
+
+router.get('/display', (req, res, next) => {
+  Places.find()
+    .then((answer) => {
+      res.render('./places/display', { answer, GMAPS: process.env.GMAPS });
+    })
+    .catch(err => console.log(`${err} fire! Fire!`));
+});
+
+router.get('/show/:id', (req, res, next) => {
+  const find = req.params.id;
+  Places.findById(find)
+    .then((answer) => {
+      res.render('./places/show', { answer, GMAPS: process.env.GMAPS });
+    })
+    .catch(err => console.log(`${err} fire! Fire!`));
+});
+
+router.get('/edit/:id', (req, res, next) => {
+  const find = req.params.id;
+  Places.findById(find)
+    .then((answer) => {
+      res.render('./places/edit', answer);
+    })
+    .catch(err => console.log(`${err} fire! Fire!`));
+});
+
+router.post('/:id', (req, res, next) => {
+  const { id } = req.params;
+  const { name, type, latitude, longitude } = req.body;
+
+  const location = {
+    type: 'Point',
+    coordinates: [parseFloat(longitude), parseFloat(latitude)],
+  }
+
+  Places.update({ _id: id }, { $set: { name, type, location } })
+    .then(() => {
+      res.redirect('./display');
+    })
+    .catch(err => console.log(`${err} fire! Fire!`));
+});
+
+router.get('/delete/:id', (req, res, next) => {
+  const find = req.params.id;
+  Places.findByIdAndRemove(find)
+    .then(() => {
+      res.redirect('/display');
+    })
+    .catch(err => console.log(`${err} fire! Fire!`));
+});
+
+
+module.exports = router;
