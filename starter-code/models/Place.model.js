de8375c54f4b8bc87216135d9e52da41f11ded51
Inma <inmanewmail@gmail.com>
2019-07-30 23:34:10 +0200

    intentando meter el mapa

--- starter-code/models/Place.model.js
+++ starter-code/models/Place.model.js
@@ -7,8 +7,14 @@ const placeSchema = new Schema ({
   type: {
     type: String,
     enum: ['coffe shop', 'bookstore']
+  },
+  location: {
+    type: { type: String },
+    coordinates: [Number]
   }
 }, { timestamps: true })
+
+placeSchema.index({ location: '2dsphere' })
 
 const Place = mongoose.model('Place', placeSchema)
 
