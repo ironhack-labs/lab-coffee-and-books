6ce7e55223774ad9921fb8663ee6601fffa64195
Rebeca Lopez <rebecalopezsaelices@MacBook-Air-de-REBECA.local>
2019-07-31 09:50:31 +0200

    iteracion 1

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,51 @@
 const express = require('express');
 const router  = express.Router();
+const Place = require('../models/place')
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  // res.render('index');
+  Place.find({})
+  .then(allPlaces=>{
+    res.render('index',{places: allPlaces});
+  })
 });
+
+router.get('/crearLocal', (req, res, next) => {
+  res.render('create');
+});
+
+
+
+
+
+router.post("/crearLocal", (req, res, next) => {
+
+  const { name, type } = req.body
+
+  if (name === "" || type === "") {
+      res.render("create", { message: "Debes rellenar todos los datos" });
+      return;
+  }
+
+  Place.findOne({ name })
+      .then(local => {
+          if (local) {
+              res.render("create", { message: "El local ya existe" });
+              return;
+          }
+
+          Place.create({ name, type })
+              .then(() => res.redirect('/'))
+              .catch(err => console.log('Hubo un error:', err))
+          })
+      
+      .catch(error => {
+          next(error)
+      })
+});
+
+
 
 module.exports = router;
