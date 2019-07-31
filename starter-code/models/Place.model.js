94cb734cc29356e2ceb803685c42a5f91588b9f2
Carla Rodriguez <carlavrodriguezm@gmail.com>
2019-07-31 08:01:47 +0200

    done

---
+++ starter-code/models/Place.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const placeSchema = new Schema({
+  name: String,
+  type: {
+    type: String,
+    enum: ['COFFEE SHOP', 'BOOKSTORE']
+  }/*,
+  location: {
+    type: { type: String },
+    coordinates: [Number]
+  }*/
+},{ timestamps: true })
+
+placeSchema.index({ location: '2dsphere' })
+
+module.exports = mongoose.model('Place', placeSchema);
