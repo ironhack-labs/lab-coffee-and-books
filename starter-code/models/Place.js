1c488395d2841c42b57dfb8ac5fb510bb955e77e
Larry Nobles <larrynobles@Larrys-MacBook-Pro.local>
2019-09-12 10:38:37 -0500

    functioning but not cute

---
+++ starter-code/models/Place.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const placeSchema = new Schema(
+  {
+    name: String,
+    type: {
+      type: String,
+      enum: ["coffeeShop", "bookStore"]
+    },
+    location: {
+      type: {
+        type: String,
+        default: "Point"
+      },
+      coordinates: [Number],
+    },
+  },
+  { timestamps: true }
+)
+
+placeSchema.index({ location: "2dsphere" })
+
+module.exports = mongoose.model("Place", placeSchema);
