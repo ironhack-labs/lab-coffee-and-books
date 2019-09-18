fcce5df83f869c8cd3db0433829d01d2c8448c99
Antonioadv08 <antonioadv08@gmail.com>
2019-09-18 09:02:15 +0200

    [MAD DE]Antonio Diez

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -1,5 +1,47 @@
-document.addEventListener('DOMContentLoaded', () => {
+const markers = [];
+var map;
+var pin;
 
-  console.log('IronGenerator JS imported successfully!');
+function startMap() {
+  var latlng = new google.maps.LatLng(40.392274, -3.697376);
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: latlng,
+    zoom: 15
+  });
+}
 
-}, false);
+
+
+  function places(place) {
+    place.forEach(point => {
+      const center = {
+        lat: Number(point.lat),
+        lng: Number(point.lng)
+      };
+      pin = new google.maps.Marker({
+        position: center,
+        map: map,
+        title: point.name
+      });
+      markers.push(pin);
+    });
+  }
+
+  function getPlace() {
+    axios
+      .get("/places/api")
+      .then(res => {
+        places(res.data);
+      })
+      .catch(err => console.log(err));
+  }
+
+  document.addEventListener(
+    "DOMContentLoaded",
+    () => {
+      startMap();
+      getPlace();
+      places();
+    },
+    false
+  );
