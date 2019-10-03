2e55a8e771d7cf7f7d526b26d736eddab430e218
grazidiandra <grazi.diandra@gmail.com>
2019-10-03 17:18:10 -0300

    done

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -1,5 +1,74 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    console.log('IronGenerator JS imported successfully!');
+  },
+  false
+);
 
-  console.log('IronGenerator JS imported successfully!');
+window.onload = () => {
+  getPlace();
+};
 
-}, false);
+function getPlace() {
+  axios
+    .get('/api')
+    .then(response => {
+      placeCoffeeBook(response.data);
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+function placeCoffeeBook(place) {
+  const markers = [];
+
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: {
+      lat: -23.566857,
+      lng: -46.6696338
+    }
+  });
+
+  place.forEach(function(place) {
+    const center = {
+      lat: place.location.coordinates[1],
+      lng: place.location.coordinates[0]
+    };
+    const pin = new google.maps.Marker({
+      position: center,
+      map: map,
+      title: place.name
+    });
+    markers.push(pin);
+  });
+}
+
+// const geocoder = new google.maps.Geocoder();
+
+// document.getElementById('submit').addEventListener('click', function () {
+//   geocodeAddress(geocoder, map);
+// });
+
+// function geocodeAddress(geocoder, resultsMap) {
+//   let address = document.getElementById('address').value;
+
+//   geocoder.geocode({ 'address': address }, function (results, status) {
+
+//     if (status === 'OK') {
+//       resultsMap.setCenter(results[0].geometry.location);
+//       let marker = new google.maps.Marker({
+//         map: resultsMap,
+//         position: results[0].geometry.location
+//       });
+//       document.getElementById('latitude').value = results[0].geometry.location.lat();
+//       document.getElementById('longitude').value = results[0].geometry.location.lng();
+//     } else {
+//       alert('Geocode was not successful for the following reason: ' + status);
+//     }
+//   });
+// }
+// });
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,59 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Place = require('../models/place');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  Place.find()
+    .then(arrPlace => {
+      res.render('index', {arrPlace});
+    })
+    .catch(err => console.log(`error: ${err}`));
+});
+
+router.get('/add', (req, res, next) => {
+  res.render('placeAdd');
+});
+
+router.post('/add', (req, res, next) => {
+  let location = {
+    type: 'Point',
+    coordinates: [req.body.longitude, req.body.latitude]
+  };
+
+  const newPlace = new Place({
+    name: req.body.name,
+    type: req.body.type,
+    location: location
+  });
+  newPlace
+    .save()
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+router.get('/api', (req, res) => {
+  Place.find()
+    .then(arrPlace => {
+      res.json(arrPlace);
+    })
+    .catch(e => {
+      throw new Error(e);
+    });
+});
+
+router.get('/api/:placeId', (req, res) => {
+  Place.findById(req.params.placeId)
+    .then(place => {
+      res.json(place);
+    })
+    .catch(e => {
+      throw new Error(e);
+    });
 });
 
 module.exports = router;
