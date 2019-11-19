370ed06dd519a4ab3e44beec6f4ad35db60952db
Manuel Álvarez Rosado <manuelalvarezrosado@MacBook-Air-de-Manuel.local>
2019-11-20 00:20:12 +0100

    Ya paro

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -1,5 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
+let theMap = new google.maps.Map(document.getElementById("map"),{
+  zoom: 10
+})
 
-  console.log('IronGenerator JS imported successfully!');
+function startMap(theMap) {
+  
+  new google.maps.Marker({
+    position: {
+      lat: {lat},
+      lng: {lon}
+    },
+    map: theMap
+  });
+}
 
-}, false);
+
+
