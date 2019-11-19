6fae469d6d2362cf3ff1e42bbf757552a1c76360
Rebecca Wood <rebecca@Rebeccas-Air.home>
2019-11-19 23:43:29 +0100

    no map

--- starter-code/routes/index.routes.js
+++ starter-code/routes/index.routes.js
@@ -1,8 +1,29 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const Places = require("../models/place.model")
 
 /* GET home page */
 router.get('/', (req, res) => res.render('index'))
 
+router.post('/', (req, res, next) => {
+
+  let location = {
+    type: 'Point',
+    coordinates: [req.body.longitude, req.body.latitude]
+  }
+
+  const newPlaces = new Places({
+    name: req.body.name,
+    type: req.body.type,
+    location
+  });
+
+  newPlaces.save((error) => {
+    if (error) {
+      next(error);
+    } else {
+      res.redirect('/');
+    }
+  });
+});
 module.exports = router
