b6b50bd1372c6551233068bf50e5956f07014c95
Rebecca Wood <rebecca@Rebeccas-MacBook-Air.local>
2019-11-20 09:14:41 +0100

    aps routes

--- starter-code/routes/index.routes.js
+++ starter-code/routes/index.routes.js
@@ -5,25 +5,51 @@ const Places = require("../models/place.model")
 /* GET home page */
 router.get('/', (req, res) => res.render('index'))
 
-router.post('/', (req, res, next) => {
+router.get('/api', (req, res, next) => {
+  Places.find()
+    .then(allPlaces => res.status(200).json({
+      Places: allPlaces
+    }))
+    .catch(err => next(err))
+});
 
-  let location = {
-    type: 'Point',
-    coordinates: [req.body.longitude, req.body.latitude]
-  }
 
-  const newPlaces = new Places({
-    name: req.body.name,
-    type: req.body.type,
-    location
-  });
 
-  newPlaces.save((error) => {
+router.get('/api/:id', (req, res, next) => {
+  let placeId = req.params.id;
+  Places.findOne({
+    _id: placeId
+  }, (error, onePlace) => {
     if (error) {
-      next(error);
+      next(error)
     } else {
-      res.redirect('/');
+      res.status(200).json({
+        restaurant: onePlace
+      });
     }
   });
 });
+
+
+// router.post('/', (req, res, next) => {
+
+//   let location = {
+//     type: 'Point',
+//     coordinates: [req.body.longitude, req.body.latitude]
+//   }
+
+//   const newPlaces = new Places({
+//     name: req.body.name,
+//     type: req.body.type,
+//     location
+//   });
+
+//   newPlaces.save((error) => {
+//     if (error) {
+//       next(error);
+//     } else {
+//       res.redirect('/');
+//     }
+//   });
+// });
 module.exports = router
