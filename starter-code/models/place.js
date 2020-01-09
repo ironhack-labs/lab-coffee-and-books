22a51977f9812ef81eba518e866775bea09ecb3a
Alex Chivescu <alexchivescu@gmail.com>
2020-01-09 16:08:09 -0300

    nas coxas

---
+++ starter-code/models/place.js
@@ -0,0 +1,46 @@
+const mongoose     = require('mongoose');
+const Schema       = mongoose.Schema;
+const permittedTypes = [`coffee shop`, `bookstore`];
+
+const placeSchema = new Schema({
+  name: {
+    type: [String]
+  },
+  type: {
+    type: [String],
+    enum: permittedTypes,
+    required: `Please specify either coffee shop or bookstore.`
+  },
+  timestamp: {
+    type: [Date]
+  }
+});
+
+
+// class APIHandler {
+//   constructor (baseUrl) {
+//     this.BASE_URL = baseUrl;
+//     this.client = axios.create({
+//       baseURL = this.BASE_URL
+//     })
+//   }
+
+//   createAxiosFunction (newPlace) {
+//     return this.client.post(this.BASE_URL, newPlace);
+//   }
+
+//   readAxiosFunction (name) {
+//     return this.client.get(`${this.BASE_URL}/${name}`);
+//   }
+
+//   updateAxiosFunction (name, editedPlace) {
+//     return this.client.put(`${this.BASE_URL}/${name}`, editedPlace);
+//   }
+
+//   deleteAxiosFunction (name) {
+//     return this.client.delete(`${this.BASE_URL}/${name}`);
+//   }
+// }
+
+
+module.exports = mongoose.model('Places', placeSchema);
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -6,4 +6,18 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+/* GET input page */
+router.get('/input', (req, res, next) => {
+  res.render('input');
+});
+
+// Get information from input page
+router.get('/input/edit', (req, res, next) => {
+});
+
+// Get information from input page
+router.post('/input/edit', (req, res, next) => {
+  placeSchema(name: this.name, type: this.`input/type`, timestamp: Date.now())
+});
+
 module.exports = router;
