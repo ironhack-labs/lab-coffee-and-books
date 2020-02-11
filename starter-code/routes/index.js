561eed6bb119f4aae8c6ff1268312bb6d49dddd1
Pedro Sanchez <pedro@pop-os.localdomain>
2020-02-11 23:55:42 +0100

    done with problems

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,78 @@
 const express = require('express');
 const router  = express.Router();
+const Place = require("../models/place.js");
+const coords=[]
 
-/* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+
+router.get("/", (req, res, next) => {
+  Place.find({})
+    .then(allPlaces =>
+      res.render("index", { places: allPlaces })
+    )
+    .catch(function() {
+      next();
+      throw new Error("There's an error.");
+    });
+});
+
+router.get('/maps/:id', (req,res) => {
+  Place.findById(req.params.id).then(place => {
+    res.render('maps', place);
+  }) 
+});
+
+router.get("/mostrar/:id", (req, res, next) => {
+  Place.findById(req.params.id)
+    .then(placeData =>
+      res.render("mostrar", { place: placeData })
+    )
+});
+
+router.get("/crear", (req, res, next) => res.render("crear"));
+router.post("/crear", (req, res, next) => {
+  Place.create({
+    name: req.body.name,
+    type: req.body.type,
+    lat: +req.body.lat,
+    lon: +req.body.lon
+  })
+    .then(() => res.redirect("/"))
+    .then((placeCreated) => coords.push(placeCreated.lon)) 
+    .then((placeCreated) => coords.push(placeCreated.lat)) 
+    .catch(function() {
+      next();
+      throw new Error("Error");
+    });
+});
+
+router.get("/:id/delete", (req, res, next) => {
+  Place.findByIdAndDelete(req.params.id)
+    .then(deletedPlace => res.redirect("/"))
+    .catch(function() {
+      next();
+      throw new Error("Error");
+    });
+});
+
+
+router.get("/:id/editar", (req, res, next) => {
+  Place.findById(req.params.id).then(place => {
+    res.render('editar', place);
+  }) 
+});
+
+router.post('/editar', (req,res) => {
+  Place.find({name:req.body.name}).then(places => {
+    if(places.length <= 0 || `${places[0]._id}` === req.body.id){
+      Place.findByIdAndUpdate(req.body.id,{
+        name:req.body.name,
+        type:req.body.type,
+        lat: req.body.lat,
+        lon: req.body.lon,
+        }).then(()=> res.redirect("/"))
+    }
+  });
+  res.redirect("/")
 });
 
 module.exports = router;
