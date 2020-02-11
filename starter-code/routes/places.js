95b926ca07fe559340c2dcebe8f85fb140622148
Carolina Guzman <carolinaguzman76@gmail.com>
2020-02-11 23:36:01 +0100

    no funciona la edicion

--- starter-code/routes/places.js
+++ starter-code/routes/places.js
@@ -28,45 +28,44 @@ router.post('/add', (req, res) => {
     .catch(err => console.log(err))
 })
 
-// // Consulta de los datos de una celebridad
-// router.get('/:id', (req, res) => {
+// Editar lugar
+router.get('/edit/:placeId', (req, res) => {
+  console.log(ENTRA)
 
-//   const celebrityId = req.params.id
+  const placeId = req.query.placeId
+  Place.findById(placeId)
+    .then(thePlace => res.render('places/edit', thePlace))
+    .catch(err => console.log(err))
+})
+router.post('/edit/:placeId', (req, res) => {
+  console.log("EL Id del lugar que llega como URL param es:", req.params.placeId)
+  const placeId = req.params.placeId
 
-//   Celebrity.findById(celebrityId)
-//     .then(celebrityDetails => res.render('celebrities/show', celebrityDetails))
-//     .catch(err => console.log("Error consultadno el libro en la BBDD: ", err))
-// })
+  Place.findByIdAndUpdate(placeId, req.body, {new: true})
+    .then(x => res.redirect(`/places/show/${placeId}`))
+    .catch(err => console.log(err))
+})
 
-// // Eliminar celebridad
-// router.post('/:id/delete', (req, res) => {
-//   const celebrityId = req.params.id
+// Consulta de los datos de un lugar
+router.get('/:id', (req, res) => {
 
-//   Celebrity.findByIdAndRemove(celebrityId)
-//     .then(() => res.redirect('/celebrities/list'))
-//     .catch(err => console.log(err))
-// })
+  const placeId = req.params.id
 
-// // Editar una celebridad
-// router.get('/:id/edit', (req, res) => {
+  Place.findById(placeId)
+    .then(placeDetails => res.render('places/show', placeDetails))
+    .catch(err => console.log("Error al consultar los lugares en la BBDD: ", err))
+})
 
-//   const celebrityId = req.query.celebrityId
+// Eliminar lugar
+router.post('/:id/delete', (req, res) => {
+  const placeId = req.params.id
 
-//   Book.findById(celebrityId)
-//     .then(editedCelebrity => res.render('celebrities/edit', editedCelebrity))
-//     .catch(err => console.log(err))
-// })
-// router.post('/:id/edit', (req, res) => {
-//   console.log("EL Id de la celebridad es:", req.params.celebrityId)
-//   const celebrityId = req.params.celebrityId
+  Place.findByIdAndRemove(placeId)
+    .then(() => res.redirect('/places/list'))
+    .catch(err => console.log(err))
+})
 
-//   // Retorna el objeto actualizado:
-//   // Book.findByIdAndUpdate(bookId, req.body, {new: true}) 
 
-//   Book.findByIdAndUpdate(celebrityId, req.body)
-//     .then(x => res.redirect(`celebrities/show/${celebrityId}`))
-//     .catch(err => console.log(err))
-// })
 
 
 module.exports = router
