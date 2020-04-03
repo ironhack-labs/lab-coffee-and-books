4db819e714df58d362698cbce827675458031d62
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-04-03 19:22:28 -0300

    Finish all routes

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -9,12 +9,102 @@ router.get('/', (req, res, next) => {
 
 
 //GET places page 
-  router.get('/places', (req, res, next) => {
-    Place.
-      find()
-      .then( response => {
-        res.render('places', {data: response});
-      })
-      .catch( error => console.log(error))
-  });
+router.get('/places', (req, res, next) => {
+  Place.
+    find()
+    .then( response => {
+      console.log(response)
+      res.render('places', {data: response});
+    })
+    .catch( error => console.log(error))
+});
+
+
+//GET places page 
+router.get('/places/new', (req, res, next) => {
+  Place.
+    find()
+    .then( response => {
+      res.render('new-place');
+    })
+    .catch( error => console.log(error))
+});
+
+//GET places page 
+router.post('/places/new', (req, res, next) => {
+
+  let { name, type } = req.body;
+
+  Place.
+    create({
+      name,
+      type
+    })
+    .then( response => {
+      res.redirect('/places');
+    })
+    .catch( error => console.log(error))
+});
+
+// DELETE request route
+router.get('/places/delete/:id', (req, res) => {
+  let { id } = req.params;
+
+  console.log(id);
+
+  Place.findByIdAndRemove(id)
+    .then( response => {
+      console.log(response);
+      res.redirect('/places');
+    })
+    .catch( error => console.log(error))
+})
+
+//EDIT route
+
+router.get('/places/edit/:id', (req, res) => {
+  let { id } = req.params;
+
+  Place.findById(id)
+    .then( response => {
+      console.log(response);
+      res.render('edit-place', { data: response });
+    })
+    .catch( error => {
+      console.log(error);
+    })
+})
+
+router.post('/places/edit/:id', (req, res) => {
+  let { id } = req.params;
+  let { name, type } = req.body;
+  console.log(req.body);
+
+  Place.findByIdAndUpdate(id, { name, type })
+    .then( response => {
+      console.log(response);
+      res.redirect('/places')
+    })
+    .catch( error => {
+      console.log(error);
+    })
+})
+//to-do
+
+
+
+
+
+
+
+// API for places
+router.get('/places/api', (req, res) => {
+  Place.find({})
+  .then( response => {
+    console.log(response);
+    res.json(response);
+  })
+  .catch( error => console.log(error));
+});
+
 module.exports = router;
