90ed6fccd08188ef348c67aa0ca1b3af91efc5d0
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-04-03 18:31:47 -0300

    Finish first route to GET places

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router  = express.Router();
+const Place = require('../model/Place');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+
+//GET places page 
+  router.get('/places', (req, res, next) => {
+    Place.
+      find()
+      .then( response => {
+        res.render('places', {data: response});
+      })
+      .catch( error => console.log(error))
+  });
 module.exports = router;
