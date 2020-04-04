5e00bb2585a2482a6fb55d2f585ec96f6c71f855
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-04-04 11:43:30 -0300

    Add styles on main places page

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -10,7 +10,7 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    zoom: 10
   });
 
   axios.get('http://localhost:3000/places/api')
