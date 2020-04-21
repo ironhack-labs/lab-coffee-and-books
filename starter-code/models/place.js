244054535de7c2e1b5768133a28453bae15db8dd
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-21 18:07:13 +0200

    add link to google api, place model

---
+++ starter-code/models/place.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const placeSchema = new Schema(
+  {
+    name: String,
+    type: { type: String, enum: ['coffee shop', 'bookstore'] },
+    location: Object,
+  },
+  { timestamps: true }
+)
+
+const Place = mongoose.model('Place', placeSchema)
+
+module.exports = Place
