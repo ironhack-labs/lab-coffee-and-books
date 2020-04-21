a1e56711a6f3abc74c71474903409f6a9f17e59c
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-21 23:23:27 +0200

    render markers on map from addresses on db

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -7,26 +7,50 @@ document.addEventListener(
 )
 const storeMap = document.getElementById('store-map')
 
+let defaultCenter
+let googleMap
+
+let markers = []
+let marker
+
+function pinMarkers(places, resultMap) {
+  const geocoder = new google.maps.Geocoder()
+  places.forEach((place) => {
+    geocoder.geocode({ address: place.address }, (results, status) => {
+      if (status === 'OK') {
+        resultMap.setCenter(results[0].geometry.location)
+        marker = new google.maps.Marker({
+          map: resultMap,
+          position: results[0].geometry.location,
+          title: place.name,
+        })
+        markers.push(marker)
+      } else {
+        console.log("Geocode wasn't successful: ", status)
+      }
+    })
+  })
+}
+
 function getPlaces() {
-  axios.get("/api")
-  .then((response) => {
-    console.log("Respuesta server: ", response)
-  }).catch((err) => {
-    console.log(err)
-  });
+  axios
+    .get('/api')
+    .then((response) => {
+      pinMarkers(response.data.places, googleMap)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
 
 function startMap() {
-  const lasSetas = {
+  defaultCenter = {
     lat: 37.3931548,
     lng: -5.9940192,
   }
-  const map = new google.maps.Map(storeMap, {
+  googleMap = new google.maps.Map(storeMap, {
     zoom: 17,
-    center: lasSetas,
+    center: defaultCenter,
   })
   getPlaces()
 }
-
-startMap()
-
