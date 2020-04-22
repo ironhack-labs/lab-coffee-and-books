1d69066a7e23553188cb7d9db323e83991d9890f
Alvaro Moral <aruvaro@gmail.com>
2020-04-22 08:58:02 +0200

    done

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -1,5 +1,35 @@
-document.addEventListener('DOMContentLoaded', () => {
+let myMap = undefined
 
-  console.log('IronGenerator JS imported successfully!');
+window.onload = () => {
 
-}, false);
+  const placeCenter = {
+    lat: 40.391074,
+    lng: -3.701793
+  };
+  let mapOptions = {
+    zoom: 15,
+    center: placeCenter
+  }
+  myMap = new google.maps.Map(document.getElementById("myMap"), mapOptions)
+
+  getPlaces()
+}
+
+function getPlaces() {
+  axios.get("/api")
+    .then(placesApi => {
+      const place = placesApi.data
+      place.forEach(elm => {
+        const center = {
+          lat: elm.location.coordinates[0],
+          lng: elm.location.coordinates[1]
+        }
+        new google.maps.Marker({
+          position: center,
+          map: myMap,
+          title: elm.name
+        })
+      })
+    })
+    .catch(error => console.log(error))
+}
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,70 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Place = require("../models/place")
 
-/* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  Place.find()
+    .then(allPlaces => res.render('index', {
+      allPlaces
+    }))
+    .catch(err => console.log("An error ocurred", err))
+})
+
+router.get("/details/:id", (req, res, next) => {
+  Place.findById(req.params.id)
+    .then(selectedPlace => res.render("details", {
+      selectedPlace
+    }))
+    .catch(err => console.log("An error ocurred", err))
+})
+
+router.get('/create', (req, res, next) => res.render('create'))
+router.post('/create', (req, res, next) => {
+  let location = {
+    type: "point",
+    coordinates: [req.body.longitude, req.body.latitude]
+  }
+  Place.create({
+      name: req.body.name,
+      type: req.body.type,
+      location
+    })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log("An error ocurred", err))
+})
+router.post('/details/:id/delete', (req, res, next) => {
+  Place.findByIdAndRemove(req.params.id)
+    .then(() => res.redirect("/"))
+    .catch(err => res.send("An error ocurred", err))
+})
+router.get("/details/:id/edit", (req, res, next) => {
+  Place.findById(req.params.id)
+    .then(editPlace => res.render("edit",
+      editPlace
+    ))
+    .catch(err => console.log("An error ocurred", err))
+})
+router.post('/details/:id/edit', (req, res, next) => {
+  let location = {
+    type: "point",
+    coordinates: [req.body.longitude, req.body.latitude]
+  }
+  Place.findByIdAndUpdate(req.params.id, {
+      name: req.body.name,
+      type: req.body.type,
+      location
+    }, {
+      new: true
+    })
+    .then(res.redirect("/"))
+    .catch(err => console.log("An error ocurred", err))
+})
+
+router.get("/api", (req, res, next) => {
+  Place.find()
+    .then(places => {
+      res.json(places);
+    })
+    .catch(error => console.log(error));
 });
-
 module.exports = router;
