ae28941e21f57b79992442bfc7998f202d223803
Ruben Narvaez <rubnarvaez@gmail.com>
2020-04-22 09:20:37 +0200

    hmm

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('node-sass-middleware')
 
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -7,11 +8,11 @@ const favicon      = require('serve-favicon');
 const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
-const path         = require('path');
+const path = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/coffe-books', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -52,7 +53,8 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 const index = require('./routes/index');
-app.use('/', index);
+
+app.use('/', index)
 
 
 module.exports = app;
