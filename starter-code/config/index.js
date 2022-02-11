59e2bdab8545eab3f216797512be1da32ebe66a8
Hibaber <hiba.berber@gmail.com>
2022-02-11 09:31:05 +0100

    lab done

---
+++ starter-code/config/index.js
@@ -0,0 +1,24 @@
+// We reuse this import in order to have access to the `body` property in requests
+const express = require("express");
+
+const logger = require("morgan");
+
+const favicon = require("serve-favicon");
+
+const path = require("path");
+
+module.exports = (app) => {
+
+  app.use(logger("dev"));
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+
+  app.set("views", path.join(__dirname, "..", "views"));
+
+  app.set("view engine", "hbs");
+
+  app.use(express.static(path.join(__dirname, "..", "public")));
+
+  app.use(favicon(path.join(__dirname, "..", "public", "images", "favicon.ico")));
+};
