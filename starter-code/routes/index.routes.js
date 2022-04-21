08219f19713337f20f7bccaec7ca84e9433e31d9
Arturo Echanique <arturoechanique@gmail.com>
2022-04-21 21:18:46 +0200

    lab solved

---
+++ starter-code/routes/index.routes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+/* GET home page */
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.use('/places', require('./places.routes'))
+router.use('/api', require('./api.routes'))
+
+module.exports = router;
