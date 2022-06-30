9ebff80b8bd3182531098f7a587d7cfe9fb8770f
Santiago Moncada <santiago.m.a@hotmail.com>
2022-06-30 21:20:06 +0200

    Added map view for creation

--- lab-coffe-and-books/public/js/geocode.js
+++ lab-coffe-and-books/public/js/geocode.js
@@ -1,14 +1,34 @@
 let geocoder;
 const key = "AIzaSyAMzL3s__ul0jf_RCKdh2eRUwJJsQ5ojTo";
 const url = "https://maps.googleapis.com/maps/api/geocode/json?";
-function initialize() {
-    geocoder = new google.maps.Geocoder();
+
+let map;
+let currentMaker;
+const coordsSOL = {
+    lat: 40.4169,
+    lng: -3.7035
 }
 
 
-document.getElementById('direction').onkeyup = event => {
+function initialize() {
+    geocoder = new google.maps.Geocoder();
+    renderMap();
+    currentMaker = new google.maps.Marker({ position: { lat: 0, lng: 0 }, map })
+}
 
-    console.log(event.currentTarget.value);
+function renderMap() {
+
+    map = new google.maps.Map(
+        document.querySelector('#smallMap'),
+        {
+            zoom: 12,
+            center: coordsSOL,
+        }
+    )
+}
+
+document.getElementById('direction').onkeyup = event => {
+    //google.maps.marker.setMap(null);
     const serch = event.currentTarget.value;
     const request = url + "address=" + serch + "&key=" + key;
     fetch(request)
@@ -19,6 +39,11 @@ document.getElementById('direction').onkeyup = event => {
             const lonBox = document.getElementById('lon');
             latBox.value = lat;
             lonBox.value = lng;
+            map.setCenter({ lat, lng });
+            currentMaker.setMap(null);
+            currentMaker = new google.maps.Marker({ position: { lat, lng }, map });
+
+
 
         })
         .catch(err => console.log(err));
