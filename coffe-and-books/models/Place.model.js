031b259c60e1b709b218ff45cc4fa3df73a3d55e
Paula <94paulala@gmail.com>
2023-05-11 22:06:33 +0200

    parcialmente resuelto

---
+++ coffe-and-books/models/Place.model.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const placeSchema = new Schema(
+  {
+    name: String,
+    type: {
+        type: String,
+        enum: ['coffee shop', 'bookstore']
+    },
+    location: {
+        type: {
+            type: String
+        },
+        coordinates: [Number]
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Place = model("Place", placeSchema);
+
+module.exports = Place;
