de76933e810148e37258ecc119eebfd5291c2ae2
Gustavo Gesto <gestogarciagustavo@gmail.com>
2023-05-12 09:20:24 +0200

    rakata

---
+++ coffe_and_books/routes/api.routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const Place = require('../models/place')
+
+router.get("/api/places", (req, res, next) => {
+
+    Place
+        .find()
+        .then(places => res.json(places))
+        .catch(err => next(err))
+});
+
+module.exports = router
