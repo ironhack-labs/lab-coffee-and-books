b790d2395047988c9a4b1f72f07316c7e1381e11
LourdesGonzalezT <LourdesGonzalezT@github.com>
2023-05-12 08:58:30 +0200

    done

---
+++ routes/placesMap.routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const Place = require('./../models/Restaurant.model')
+
+router.get("/places", (req, res, next) => {
+
+    Place
+        .find()
+        .then(places => res.json(places))
+        .catch(err => next(err))
+});
+
+
+
+module.exports = router
